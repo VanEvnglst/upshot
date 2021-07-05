485f975713b715901e92a77bed7720ebdc36c8e1
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-07-05 15:50:44 +0800

    Create initial saga for authentication

--- app/sagas/authentication-saga.js
+++ app/sagas/authentication-saga.js
@@ -1,9 +1,20 @@
-import {call, put, takeLatest, select} from 'redux-saga/effects';
+import { checkInternetConnection } from 'react-native-offline';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
 import AuthenticationActions, {
   AuthenticationTypes,
 } from '../store/authentication-redux';
 
-export function* signInUser() {}
+export function* signInUser() {
+  const connected = yield checkInternetConnection();
+  if (!connected) {
+    //yield showNetworkError();
+    return;
+  }
+  const authResponse = yield call(api.signInUser, { email, password });
+  if (authResponse.ok) {
+    console.log('auth ok');
+  }
+}
 
 function* watchAuthenticationSaga() {
   yield takeLatest(AuthenticationTypes.SIGN_IN_USER, signInUser);
