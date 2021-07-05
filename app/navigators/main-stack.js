82162b3aaae2e4b9d83854ae28dbd4796f26de06
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-07-05 15:50:02 +0800

    Remove comment from stack navigator

--- app/navigators/main-stack.js
+++ app/navigators/main-stack.js
@@ -3,7 +3,6 @@ import React from 'react';
 import TabRoutes from './tab-routes';
 
 export default function (Stack) {
-  console.warn('This is the main stack');
   return (
     <>
       <Stack.Screen
