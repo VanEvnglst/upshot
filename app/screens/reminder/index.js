496145d0f0b2017986f9db422c2218e07e9c9dc2
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-08-23 17:20:17 +0800

    Create reminders screen

--- app/screens/reminder/index.js
+++ app/screens/reminder/index.js
@@ -1,19 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import styles from './styles';
 import { Wrapper } from '../../components';
+import Images from '../../assets/images';
+import labels from '../../locales/en';
 
 const ReminderScreen = () => {
+  const { triviaReminder, morningReminder, morningContent } = labels.reminders;
+  const [currentTime, setTime] = useState(new Date().getHours());
+  const [content, setContent] = useState({
+    image: '',
+    title: '',
+    content: '',
+  });
+
+  handleContent = () => {
+    if (currentTime >= 15) {
+      setContent({
+        image: Images.pmReminder,
+        title: triviaReminder,
+        content: morningContent,
+      });
+    } else {
+      setContent({
+        image: Images.amReminder,
+        title: morningReminder,
+        content: morningContent,
+      });
+    }
+  };
+
+  useEffect(() => {
+    handleContent();
+  }, [currentTime]);
+
   return (
     <Wrapper>
-      <View style={{ flex: 1, backgroundColor: 'red' }}></View>
-      <Text style={{ fontSize: 32, margin: 20 }}>
-        Today is a new day, User!
-      </Text>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Content area</Text>
+      <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+        <Image source={content.image} resizeMode="contain" />
       </View>
-      <Text>Call to Action</Text>
+      <View style={{ flex: 1 }}>
+        <Text style={{ fontSize: 32, marginBottom: 20 }}>{content.title}</Text>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Text>{content.content}</Text>
+        </View>
+      </View>
+      <Text>Give Feedback</Text>
     </Wrapper>
   );
 };
