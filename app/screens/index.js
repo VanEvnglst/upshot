4d2ee9db498ada60c7d383f8b48b39289db0171a
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-08-23 16:53:47 +0800

    Add Feedback to screens list

--- app/screens/index.js
+++ app/screens/index.js
@@ -2,9 +2,12 @@
 export { default as SignIn } from './sign-in';
 export { default as Onboarding } from './onboarding';
 
-//Main Stack
+//Main Tabs
 export { default as Home } from './home';
 export { default as Messages } from './messages';
 export { default as Activity } from './activity';
 export { default as Profile } from './profile';
 export { default as Reminders } from './reminder';
+
+// Feedback Stack
+export { default as FeedbackPreparation } from './feedback-preparation';
