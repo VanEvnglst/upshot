ea8024122bafd548342a1dbf8031747fe6388191
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-08-23 16:52:51 +0800

    Add assets to project

---
+++ app/assets/images.js
@@ -0,0 +1,13 @@
+export default {
+  feedbackCoaching: require('./images/feedback-coaching.png'),
+  amReminder: require('./images/am-reminder.png'),
+  pmReminder: require('./images/pm-reminder.png'),
+  preparationStep1: require('./images/preparation-step1.png'),
+  preparationStep2: require('./images/preparation-step2.png'),
+  preparationStep3: require('./images/preparation-step3.png'),
+  preparationStep4: require('./images/preparation-step4.png'),
+  preparationStep5: require('./images/preparation-step5.png'),
+  simpleStories: require('./images/simple-stories.png'),
+  rightness: require('./images/rightness.png'),
+  agreeableness: require('./images/agreeableness.png'),
+};
