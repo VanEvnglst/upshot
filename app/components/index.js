ae159d274b4af3108e9036a764e752dd2289eb3c
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-08-25 17:35:12 +0800

    Add header component to list

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,8 @@
 export { default as LearningCard } from './learning-card';
 export { default as Wrapper } from './wrapper';
 export { default as ButtonSelection } from './button-selection';
+export { default as Chip } from './chip';
+export { default as InputField } from './input-field';
+export { default as Button } from './button';
+export { default as HintIndicator } from './hint-indicator';
+export { default as Header } from './header';
