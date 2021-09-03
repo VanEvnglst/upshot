2945b3853abdba37ac9d74918de45d49da0d0320
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-03 16:20:03 +0800

    Create documenting step 4 front end

---
+++ app/screens/feedback/documenting/step4/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { Wrapper } from '../../../../components';
+
+const DocumentingStep4 = props => {
+  return <Wrapper></Wrapper>;
+};
+
+export default DocumentingStep4;
