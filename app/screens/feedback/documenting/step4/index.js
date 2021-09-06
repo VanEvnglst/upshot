6daa0938a221f5851c58ae1998d568b25b7ffa43
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-06 15:41:56 +0800

    Create documenting step 4 screen

--- app/screens/feedback/documenting/step4/index.js
+++ app/screens/feedback/documenting/step4/index.js
@@ -1,9 +1,30 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
-import { Wrapper } from '../../../../components';
+import { ButtonSelection } from '../../../../components';
+import labels from '../../../../locales/en';
+import styles from './styles';
 
 const DocumentingStep4 = props => {
-  return <Wrapper></Wrapper>;
+  return (
+    <View style={styles.container}>
+      {/* <Text>{labels.feedbackDocumenting.feedbackToGive}</Text> */}
+      <ButtonSelection
+        title={labels.common.today}
+        type={'Radio'}
+        // content={item.hint}
+        // showHint={hint}
+        onPress={() => console.log}
+        selected={false}
+        key={i}
+      />
+      <ButtonSelection 
+        title={labels.common.yesterday}
+        type={'Radio'}
+      />
+      {/* Create new button to for calendar view */}
+      <HintIndicator showHint={hint} onPress={() => showHint(!hint)} />
+    </View>
+  );
 };
 
 export default DocumentingStep4;
