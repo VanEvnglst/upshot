533a7f1b02821b8a7cff1c7511baaea20331cfde
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-13 16:30:25 +0800

    Create font standards

---
+++ app/theme/fonts.js
@@ -0,0 +1,44 @@
+const size = {
+  h1: 96,
+  h2: 60,
+  h3: 48,
+  h4: 34,
+  h5: 24,
+  h6: 20,
+  body1: 16,
+  body2: 14,
+  caption: 12,
+  overline: 10,
+};
+
+const letterSpacing = {
+  h1: -15,
+  h2: -8,
+  h3: 0,
+  h4: 0,
+  h5: 0.075,
+  h6: 0.075,
+  subtitle1: 0.09,
+  subtitle2: 0.07,
+  button: 0.89,
+  body1: 0.31,
+  body2: 0.18,
+  caption: 0.33,
+  overline: 1.5,
+};
+
+const fontFamily = {
+  RalewayThin: 'Raleway-Thin',
+  RalewayRegular: 'Raleway-Regular',
+  RalewayMedium: 'Raleway-Medium',
+  RalewaySemiBold: 'Raleway-SemiBold',
+  RalewayBold: 'Raleway-Bold',
+  RalewayBlack: 'Raleway-Black',
+  Shrikhand: 'Shrikhand-Regular',
+};
+
+export default {
+  fontFamily,
+  size,
+  letterSpacing,
+};
