c6d6890f03ec8c09c94c0ffe817aea28961d0612
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-18 17:06:41 +0800

    Create colors file

---
+++ app/theme/colors.js
@@ -0,0 +1,9 @@
+export default {
+  white: '#FFFFFF',
+  black: '#000000',
+  red: '#B00020',
+  primary: '#6200EE',
+  primaryDark: '#3700B3',
+  secondary: '#03DAC5',
+  secondaryDark: '#018786',
+}
