12e5f659da527b720a0e292f8900e5c3e081f289
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-20 10:30:07 +0800

    Update feedback flow style

--- app/screens/feedback/introduction/index.js
+++ app/screens/feedback/introduction/index.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { useDispatch } from 'react-redux';
 import {
   Wrapper,
   Button,
   ButtonSelection,
   HintIndicator,
-  Header
+  Header,
+  Text,
 } from '../../../components';
 import discussionTypes from '../../../enums/discussion-type';
 import styles from './styles';
 import labels from '../../../locales/en';
 
-const FeedbackIntroduction = () => {
+const FeedbackIntroduction = props => {
+  const { navigation } = props;
+  const dispatch = useDispatch();
   const [discussion, selectDiscussion] = useState({
     id: null,
     title: '',
@@ -19,35 +23,51 @@ const FeedbackIntroduction = () => {
   });
   const [hint, showHint] = useState(false);
 
-  const _handleSelection = (item) => {
+  const _handleSelection = item => {
     selectDiscussion(item);
   };
 
+  const proceedToNextStep = () => {
+    // console.log('discussion', discussion);
+    // dispatch({
+    //   type: 'setFeedbackFlow',
+    //   payload: discussion,
+    // });
+    navigation.navigate('FeedbackGuide');
+  };
+
   return (
-      <Wrapper>
-        <Header headerLeft />
-        <View style={styles.contentContainer}>
-          <Text style={styles.labelPadding}>
-            {labels.feedbackIntro.action}
-          </Text>
-          {discussionTypes.map((item, i) => (
-            <ButtonSelection
-              title={item.title}
-              type={'Radio'}
-              content={item.hint}
-              showHint={hint}
-              onPress={() => _handleSelection(item)}
-              selected={item.id === discussion.id}
-            />
-          ))}
-          <HintIndicator showHint={hint} onPress={() => showHint(!hint)} />
-        </View>
-        <View style={styles.btnContainer}>
-          <Button onPress={() => console.log('nav')} block disabled={discussion.id === null}>
-            Continue
-          </Button>
-        </View>
-      </Wrapper>
+    <Wrapper>
+      <Header
+        headerLeft={{
+          onPress: () => navigation.goBack(),
+        }}
+      />
+      <View style={styles.contentContainer}>
+        <Text type="h6" style={styles.labelPadding}>
+          {labels.feedbackIntro.action}
+        </Text>
+        {discussionTypes.map((item, i) => (
+          <ButtonSelection
+            title={item.title}
+            type={'Radio'}
+            content={item.hint}
+            showHint={hint}
+            onPress={() => _handleSelection(item)}
+            selected={item.id === discussion.id}
+          />
+        ))}
+        <HintIndicator showHint={hint} onPress={() => showHint(!hint)} />
+      </View>
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={() => proceedToNextStep()}
+          block
+          disabled={discussion.id === null}>
+          Continue
+        </Button>
+      </View>
+    </Wrapper>
   );
 };
 
