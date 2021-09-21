7a90a67c1f916eb67b42e9fcafdf8024f4f37c41
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-21 17:15:01 +0800

    Add new screens to list

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,15 +1,16 @@
-//Auth Stack
+/* ------------- Auth Stack ------------- */
 export { default as SignIn } from './sign-in';
 export { default as Onboarding } from './onboarding';
 
-//Main Tabs
+/* ------------- Home Stack ------------- */
 export { default as Home } from './home';
-export { default as Messages } from './messages';
-export { default as Activity } from './activity';
+export { default as Messages  } from './messages';
 export { default as Profile } from './profile';
 export { default as Reminders } from './reminder';
 
-// Feedback Stack
-export { default as FeedbackIntroduction } from './feedback/introduction';
+/* ------------- Feedback Stack ------------- */
+export { default as FeedbackFlow } from './feedback/flow';
 export { default as FeedbackGuide } from './feedback/guide';
 export { default as FeedbackDocumenting } from './feedback/documenting';
+export { default as FeedbackSignPost} from './feedback/sign-post';
+export { default as ActiveFeedbackJourney} from './feedback/journeys';
