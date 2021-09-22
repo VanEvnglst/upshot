0d60567fe65aab722c95d822d657c28a3a028b8d
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-22 16:30:55 +0800

    Handle feedback flow redux

--- app/store/feedbackRedux.js
+++ app/store/feedbackRedux.js
@@ -9,85 +9,103 @@ const defaultState = {
 /* ------------- Initial State ------------- */
 export const INITIAL_STATE = Map({
   currentJourney: {
-    ...defaultState,
     documenting: { ...defaultState },
     preparing: { ...defaultState },
     teamMembers: { ...defaultState },
-    relatedTopics: { ...defaultState },
-    feedbackType: { ...defaultState },
-    feedbackFlow: { ...defaultState },
   },
+  feedbackFlow: { ...defaultState },
+  feedbackType: { ...defaultState },
+  relatedTopics: { ...defaultState },
   journeys: { ...defaultState },
 });
 
 /* ------------- Types and Action Creators ------------- */
 const { Types, Creators } = createActions({
-  fetchFeedbackFlow: [''],
-  fetchFeedbackFlowSuccess: null,
+  fetchFeedbackFlow: null,
+  fetchFeedbackFlowSuccess: ['feedbackFlowList'],
   fetchFeedbackFlowFailure: ['error'],
   fetchFeedbackType: [''],
-  fetchFeedbackTypeSuccess: null,
-  fetchFeedbackTypeFailure: ['error'],
-  fetchFeedbackTopics: [''],
-  fetchFeedbackTopicsSuccess: null,
-  fetchFeedbackTopicsFailure: ['error'],
-  fetchTeamMembers: [''],
-  fetchTeamMembersSuccess: null,
-  fetchTeamMembersFailure: ['error'],
-  postFeedbackJourney: [''],
-  postFeedbackJourneySuccess: null,
-  postFeedbackJourneyFailure: ['error'],
-  postFeedbackDocumenting: [''],
-  postFeedbackDocumentingSuccess: null,
-  postFeedbackDocumentingFailure: ['error'],
-  updateFeedbackDocumenting: [''],
+  // fetchFeedbackTypeSuccess: null,
+  // fetchFeedbackTypeFailure: ['error'],
+  // fetchFeedbackTopics: [''],
+  // fetchFeedbackTopicsSuccess: null,
+  // fetchFeedbackTopicsFailure: ['error'],
+  // fetchTeamMembers: [''],
+  // fetchTeamMembersSuccess: null,
+  // fetchTeamMembersFailure: ['error'],
+  // postFeedbackJourney: [''],
+  // postFeedbackJourneySuccess: null,
+  // postFeedbackJourneyFailure: ['error'],
+  // postFeedbackDocumenting: [''],
+  // postFeedbackDocumentingSuccess: null,
+  // postFeedbackDocumentingFailure: ['error'],
+  // updateFeedbackDocumenting: [''],
 });
 
 export const FeedbackTypes = Types;
 export default Creators;
 
 /* ------------- Reducers ------------- */
-const fetchFeedbackFlow = state => state.merge({});
-const fetchFeedbackFlowSuccess = state => state.merge({});
+const fetchFeedbackFlow = state =>
+  state.merge({
+    feedbackFlow: {
+      fetching: true,
+    },
+  });
+const fetchFeedbackFlowSuccess = (state, { feedbackFlowList }) => {
+  debugger;
+  state.merge({
+    feedbackFlow: {
+      fetching: false,
+      data: feedbackFlowList,
+    },
+  });
+};
 const fetchFeedbackFlowFailure = state => state.merge({});
-const fetchFeedbackType = state => state.merge({});
-const fetchFeedbackTypeSuccess = state => state.merge({});
-const fetchFeedbackTypeFailure = state => state.merge({});
-const fetchFeedbackTopics = state => state.merge({});
-const fetchFeedbackTopicsSuccess = state => state.merge({});
-const fetchFeedbackTopicsFailure = state => state.merge({});
-const fetchTeamMembers = state => state.merge({});
-const fetchTeamMembersSuccess = state => state.merge({});
-const fetchTeamMembersFailure = state => state.merge({});
-const postFeedbackJourney = state => state.merge({});
-const postFeedbackJourneySuccess = state => state.merge({});
-const postFeedbackJourneyFailure = state => state.merge({});
-const postFeedbackDocumenting = state => state.merge({});
-const postFeedbackDocumentingSuccess = state => state.merge({});
-const postFeedbackDocumentingFailure = state => state.merge({});
-const updateFeedbackDocumenting = state => state.merge({});
+const fetchFeedbackType = state =>
+  state.merge({
+    feedbackType: {
+      fetching: true,
+    },
+  });
+
+// const fetchFeedbackTypeSuccess = state => state.merge({});
+// const fetchFeedbackTypeFailure = state => state.merge({});
+// const fetchFeedbackTopics = state => state.merge({});
+// const fetchFeedbackTopicsSuccess = state => state.merge({});
+// const fetchFeedbackTopicsFailure = state => state.merge({});
+// const fetchTeamMembers = state => state.merge({});
+// const fetchTeamMembersSuccess = state => state.merge({});
+// const fetchTeamMembersFailure = state => state.merge({});
+// const postFeedbackJourney = state => state.merge({});
+// const postFeedbackJourneySuccess = state => state.merge({});
+// const postFeedbackJourneyFailure = state => state.merge({});
+// const postFeedbackDocumenting = state => state.merge({});
+// const postFeedbackDocumentingSuccess = state => state.merge({});
+// const postFeedbackDocumentingFailure = state => state.merge({});
+// const updateFeedbackDocumenting = state => state.merge({});
 
 /* ------------- Hookup Reducers To Types ------------- */
-export const feedbackReducers = createReducer(INITIAL_STATE, {
+export const reducer = createReducer(INITIAL_STATE, {
   [Types.FETCH_FEEDBACK_FLOW]: fetchFeedbackFlow,
   [Types.FETCH_FEEDBACK_FLOW_SUCCESS]: fetchFeedbackFlowSuccess,
   [Types.FETCH_FEEDBACK_FLOW_FAILURE]: fetchFeedbackFlowFailure,
   [Types.FETCH_FEEDBACK_TYPE]: fetchFeedbackType,
-  [Types.FETCH_FEEDBACK_TYPE_SUCCESS]: fetchFeedbackTypeSuccess,
-  [Types.FETCH_FEEDBACK_TYPE_FAILURE]: fetchFeedbackTypeFailure,
-  [Types.FETCH_FEEDBACK_TOPICS]: fetchFeedbackTopics,
-  [Types.FETCH_FEEDBACK_TOPICS_SUCCESS]: fetchFeedbackTopicsSuccess,
-  [Types.FETCH_FEEDBACK_TOPICS_FAILURE]: fetchFeedbackTopicsFailure,
-  [Types.FETCH_TEAM_MEMBERS]: fetchTeamMembers,
-  [Types.FETCH_TEAM_MEMBERS_SUCCESS]: fetchTeamMembersSuccess,
-  [Types.FETCH_TEAM_MEMBERS_FAILURE]: fetchTeamMembersFailure,
-  [Types.POST_FEEDBACK_JOURNEY]: postFeedbackJourney,
-  [Types.POST_FEEDBACK_JOURNEY_SUCCESS]: postFeedbackJourneySuccess,
-  [Types.POST_FEEDBACK_JOURNEY_FAILURE]: postFeedbackJourneyFailure,
-  [Types.POST_FEEDBACK_DOCUMENTING]: postFeedbackDocumenting,
-  [Types.POST_FEEDBACK_DOCUMENTING_SUCCESS]: postFeedbackDocumentingSuccess,
-  [Types.POST_FEEDBACK_DOCUMENTING_FAILURE]: postFeedbackDocumentingFailure,
-  [Types.UPDATE_FEEDBACK_DOCUMENTING]: updateFeedbackDocumenting,
+  // [Types.FETCH_FEEDBACK_TYPE_SUCCESS]: fetchFeedbackTypeSuccess,
+  // [Types.FETCH_FEEDBACK_TYPE_FAILURE]: fetchFeedbackTypeFailure,
+  // [Types.FETCH_FEEDBACK_TOPICS]: fetchFeedbackTopics,
+  // [Types.FETCH_FEEDBACK_TOPICS_SUCCESS]: fetchFeedbackTopicsSuccess,
+  // [Types.FETCH_FEEDBACK_TOPICS_FAILURE]: fetchFeedbackTopicsFailure,
+  // [Types.FETCH_TEAM_MEMBERS]: fetchTeamMembers,
+  // [Types.FETCH_TEAM_MEMBERS_SUCCESS]: fetchTeamMembersSuccess,
+  // [Types.FETCH_TEAM_MEMBERS_FAILURE]: fetchTeamMembersFailure,
+  // [Types.POST_FEEDBACK_JOURNEY]: postFeedbackJourney,
+  // [Types.POST_FEEDBACK_JOURNEY_SUCCESS]: postFeedbackJourneySuccess,
+  // [Types.POST_FEEDBACK_JOURNEY_FAILURE]: postFeedbackJourneyFailure,
+  // [Types.POST_FEEDBACK_DOCUMENTING]: postFeedbackDocumenting,
+  // [Types.POST_FEEDBACK_DOCUMENTING_SUCCESS]: postFeedbackDocumentingSuccess,
+  // [Types.POST_FEEDBACK_DOCUMENTING_FAILURE]: postFeedbackDocumentingFailure,
+  // [Types.UPDATE_FEEDBACK_DOCUMENTING]: updateFeedbackDocumenting,
 });
 
 /* ------------- Selectors ------------- */
