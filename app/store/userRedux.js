fe9b4ab30048442cb3b36cd483e07104ef1decde
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-23 20:16:59 +0800

    Create user redux

--- app/store/userRedux.js
+++ app/store/userRedux.js
@@ -2,20 +2,23 @@ import { createReducer, createActions } from 'reduxsauce';
 import { Map } from 'immutable';
 
 export const INITIAL_STATE = Map({
-  firstName: 'Ivan',
+  user: {},
 });
 
 const { Types, Creators } = createActions({
+  setUser: ['user'],
   setPassword: [''],
 });
 
 export const UserTypes = Types;
 export default Creators;
 
+const setUser = (state, { user }) => state.merge({ user });
 const setPassword = state => state.merge({});
 
 export const userReducers = createReducer(INITIAL_STATE, {
   [Types.SET_PASSWORD]: setPassword,
+  [Types.SET_USER]: setUser,
 });
 /* ------------- Selectors ------------- */
-export const getFirstName = state => state.user.firstName;
+export const getUser = state => state.user;
