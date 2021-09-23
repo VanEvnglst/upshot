0e73327aab77275948aa2564bf1d33114c291d2b
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-23 20:15:19 +0800

    Create user saga

---
+++ app/sagas/userSaga.js
@@ -0,0 +1,13 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import UserActions, { UserTypes } from '../store/userRedux';
+
+export function* setUser() {
+  const user = { firstName: 'Ivan' };
+  yield put(UserActions.setUser(user));
+}
+
+function* watchUserSaga() {
+  yield takeLatest(UserTypes.SET_USER, setUser);
+}
+
+export default watchUserSaga;
