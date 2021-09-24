91f31f6094115bd733bd662ea1bcf1818360cfd4
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-24 20:20:43 +0800

    Update documenting design

--- app/screens/feedback/documenting/index.js
+++ app/screens/feedback/documenting/index.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import { Button } from 'react-native-paper';
 import PropTypes from 'prop-types';
-import { Wrapper, ProgressIndicator, Header } from '../../../components';
+import { Wrapper, ProgressIndicator, Header, Text } from '../../../components';
+import DocumentingStep4 from './step4';
 import DocumentingStep3 from './step3';
+import DocumentingStep2 from './step2';
+import DocumentingStep1 from './step1';
 import styles from './styles';
 
 const FeedbackDocumenting = () => {
   const [currentStep, setCurrentStep] = useState(0);
   return (
-    <ScrollView>
     <Wrapper>
-      <Header headerRight />
-      <Text>Documenting</Text>
-      <ProgressIndicator steps={3} currentIndex={currentStep} />
-      <DocumentingStep3 />
-      <View style={styles.btnContainer}>
-        <Button mode="text">Back</Button>
-        <Button mode="contained">Next</Button>
-      </View>
+      <ScrollView
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ backgroundColor: 'white' }}>
+        <Header headerRight />
+        <Text type="overline">Documenting</Text>
+        <ProgressIndicator steps={4} currentIndex={currentStep} />
+        <DocumentingStep4 />
+        <View style={styles.btnContainer}>
+          <Button mode="text">Back</Button>
+          <Button mode="contained">Next</Button>
+        </View>
+      </ScrollView>
     </Wrapper>
-    </ScrollView>
   );
 };
 
