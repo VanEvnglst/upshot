1b16e32add85dc35e3f925e9a4fc27d5552250a2
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-30 19:09:32 +0800

    Create initial active journeys page

--- app/screens/feedback/journeys/index.js
+++ app/screens/feedback/journeys/index.js
@@ -1,14 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
-import { Wrapper } from '../../../components';
+import { View, ScrollView } from 'react-native';
+import {
+  Wrapper,
+  Text,
+  SignPostIndicator,
+  Header,
+  JourneyIndicator,
+} from '../../../components';
+import feedbackJourneySteps from '../../../enums/feedback-journey';
+import labels from '../../../locales/en';
 import styles from './styles';
 
-const FeedbackJourneys = () => {
-  return(
+const ActiveFeedbackJourney = () => {
+  return (
     <Wrapper>
-      <Text>Journeys and history page</Text>
+      <ScrollView>
+        <Header headerLeft />
+        <View style={{ flex: 1, marginBottom: 20 }}>
+          <Text type="h4">Ferdie A</Text>
+        </View>
+        <View style={{ flex: 4 }}>
+          {feedbackJourneySteps.map((item, i) => {
+            return (
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <SignPostIndicator
+                  isLastItem={i === feedbackJourneySteps.length - 1}
+                  isCompleted={item.id === 1}
+                  disabled={item.id > 2}
+                  current={item.id === 2}
+                />
+                <JourneyIndicator
+                  style={{ flex: 2 }}
+                  disabled={item.id > 2}
+                  done={item.id === 1}
+                  current={item.id === 2}
+                  item={item}
+                />
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
     </Wrapper>
   );
-}
+};
 
-export default FeedbackJourneys;
+export default ActiveFeedbackJourney;
