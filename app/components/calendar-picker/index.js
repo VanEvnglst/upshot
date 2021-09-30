103789ff992f4c31e714c9990ed6da1544b8af46
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-30 17:46:57 +0800

    Create calendar picker button

--- app/components/calendar-picker/index.js
+++ app/components/calendar-picker/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Image } from 'react-native';
 import { Text } from '../../components';
-const CalendarPicker = () => {
+import Images from '../../assets/images';
+import styles from './styles';
+
+const CalendarPicker = ({ onPress, text }) => {
   return (
-    <TouchableOpacity>
-      <View
-        style={{
-          backgroundColor: 'rgba(0,0,0,0.04)',
-          height: 60,
-          borderTopLeftRadius: 4,
-          borderTopRightRadius: 4,
-          borderBottomColor: 'rgba(0,0,0,0.87)',
-          borderBottomWidth: 1,
-          alignItems: 'center',
-          flexDirection: 'row',
-          paddingHorizontal: 16,
-          justifyContent: 'space-between',
-        }}>
-        <Text type="subtitle1">On a different date</Text>
-        <View style={{ width: 24, height: 24, backgroundColor: 'red' }} />
+    <TouchableOpacity accessibilityRole="button" onPress={onPress}>
+      <View style={styles.container}>
+        <Text type="subtitle1">
+          {text === '' ? `On a different date` : text}
+        </Text>
+        <Image source={Images.calendar} />
       </View>
     </TouchableOpacity>
   );
--- app/components/calendar-picker/styles.js
+++ app/components/calendar-picker/styles.js
@@ -3,6 +3,15 @@ import Colors from '../../theme/colors';
 
 export default styles = StyleSheet.create({
   container: {
-   height: 60,
+    height: 60,
+    backgroundColor: 'rgba(0,0,0,0.04)',
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    borderBottomColor: 'rgba(0,0,0,0.87)',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    justifyContent: 'space-between',
   },
 });
