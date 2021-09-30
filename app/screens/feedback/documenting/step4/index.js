11b3b799b7efce98ec3e6314eb1f96044ccb8182
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-09-30 19:04:54 +0800

    Add date picker to documenting step 4

--- app/screens/feedback/documenting/step4/index.js
+++ app/screens/feedback/documenting/step4/index.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
-import { ButtonSelection, HintIndicator } from '../../../../components';
+import { useDispatch } from 'react-redux';
+import {
+  ButtonSelection,
+  HintIndicator,
+  CalendarPicker,
+  DateTimePicker,
+} from '../../../../components';
+import FeedbackActions from '../../../../store/feedbackRedux';
 import labels from '../../../../locales/en';
 import styles from './styles';
 
 const DocumentingStep4 = props => {
+  const dispatch = useDispatch();
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [dateSelected, setDate] = useState();
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = date => {
+    console.log('date picked', date);
+    hideDatePicker();
+  };
+
   return (
     <View style={styles.container}>
       {/* <Text>{labels.feedbackDocumenting.feedbackToGive}</Text> */}
@@ -17,7 +41,13 @@ const DocumentingStep4 = props => {
         selected={false}
       />
       <ButtonSelection title={labels.common.yesterday} type={'Radio'} />
-      {/* Create new button to for calendar view */}
+      <CalendarPicker onPress={() => showDatePicker()} />
+      <DateTimePicker
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
       <HintIndicator showHint={false} onPress={() => showHint(!hint)} />
     </View>
   );
