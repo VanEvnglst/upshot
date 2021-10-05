e30fa07700b1f48cfe01507959c20df8ea93320b
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-05 14:57:38 +0800

    Create preparing step 2 screen

--- app/screens/feedback/preparing/step2/index.js
+++ app/screens/feedback/preparing/step2/index.js
@@ -1,9 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { Wrapper } from '../../../../components';
+import { View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { Wrapper, Text, ButtonSelection } from '../../../../components';
+import labels from '../../../../locales/en';
+import styles from './styles';
 
 const PreparingStep2 = props => {
-  return <Wrapper></Wrapper>;
+  const dispatch = useDispatch();
+  const { statePurpose } = labels.feedbackPreparing;
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text type="h6">
+          {statePurpose.step2}: {statePurpose.title}
+        </Text>
+        <Text type="body1" style={styles.descriptionText}>
+          {statePurpose.content}
+        </Text>
+      </View>
+      <ButtonSelection
+        type={'Radio'}
+        title={statePurpose.statePurposeBtn}
+        onPress={() => console.log('press')}
+        selected={false}
+      />
+    </View>
+  );
 };
 
 export default PreparingStep2;
---
+++ app/screens/feedback/preparing/step2/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../../../theme/colors';
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {},
+  descriptionText: {
+    lineHeight: 24,
+    marginTop: 20,
+    color: Colors.mediumBlack,
+  },
+});
