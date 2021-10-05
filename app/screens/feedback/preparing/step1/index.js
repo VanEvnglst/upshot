4380c5e28f3a935157b1769fcafe6156d7c2669b
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-05 14:57:29 +0800

    Create preparing step 1 screen

--- app/screens/feedback/preparing/step1/index.js
+++ app/screens/feedback/preparing/step1/index.js
@@ -1,9 +1,53 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { Wrapper } from '../../../../components';
+import { View, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
+import { Text } from '../../../../components';
+import labels from '../../../../locales/en';
+import styles from './styles';
 
 const PreparingStep1 = props => {
-  return <Wrapper></Wrapper>;
+  const { checkIn } = labels.feedbackPreparing;
+  const dispatch = useDispatch();
+  const [checkInValue, setCheckInValue] = useState();
+  const [isComplete, setComplete] = useState(false);
+  const [isFocused, setFocus] = useState(false);
+
+  const handleText = () => {
+    //dispatch()
+  };
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <KeyboardAvoidingView>
+        <View>
+          <View style={styles.contentContainer}>
+            <Text type="h6">
+              {checkIn.step}: {checkIn.title}
+            </Text>
+            <Text type="body1" style={styles.descriptionText}>
+              {checkIn.content}
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 30 }}>
+            <TextInput
+              type="flat"
+              label={isFocused ? checkIn.checkInHint : null}
+              placeholder={isFocused ? null : checkIn.checkInHint}
+              value={checkInValue}
+              onChangeText={text => setCheckInValue(text)}
+              textAlignVertical="top"
+              multiline
+              numberOfLines={5}
+              onBlur={() => setFocus(false)}
+              onFocus={() => setFocus(true)}
+              theme={{ fonts: { regular: { fontFamily: 'Raleway-Regular' } } }}
+            />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
 };
 
 export default PreparingStep1;
---
+++ app/screens/feedback/preparing/step1/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../../../theme/colors';
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    paddingRight: 15,
+  },
+  descriptionText: {
+    lineHeight: 24,
+    marginTop: 20,
+    color: Colors.mediumBlack,
+  },
+});
