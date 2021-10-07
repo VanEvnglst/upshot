d095829096ee22e733f6d7ba3db89c2c760deaa1
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-07 09:23:44 +0800

    Fix methods inside step 1

--- app/screens/feedback/documenting/step1/index.js
+++ app/screens/feedback/documenting/step1/index.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
-import { Chip } from 'react-native-paper';
-import {Text} from '../../../../components';
+import { useDispatch, useSelector } from 'react-redux';
+import { Chip, Button } from 'react-native-paper';
+import { Text } from '../../../../components';
+import FeedbackActions from '../../../../store/feedbackRedux';
+import DocumentingActions from '../../../../store/feedback/documentingRedux';
 import labels from '../../../../locales/en';
 import styles from './styles';
 
 const DocumentingStep1 = props => {
+  const dispatch = useDispatch();
+
+  //TODO: Fix selector
+  // const currentStep = useSelector(state => state.documenting.get('activeStep'r));
+
+  useEffect(() => {
+    // dispatch(FeedbackActions.fetchTeamMembers)
+  }, []);
+
+  const handleNext = () => {
+    console.log('1231241', currentStep);
+    dispatch(DocumentingActions.setActiveStep(currentStep));
+  };
+
   const names = [
     {
       name: 'Teammate',
@@ -26,19 +43,27 @@ const DocumentingStep1 = props => {
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
-        <Text type='h6' style={styles.label}>{labels.feedbackDocumenting.giveFeedbackTo}</Text>
+        <Text type="h6" style={styles.label}>
+          {labels.feedbackDocumenting.giveFeedbackTo}
+        </Text>
+
         <View style={styles.namesContainer}>
           {names.map((item, i) => (
-            <Chip 
-              mode="flat" 
-              style={styles.chips}
-            >
-              <Text type='body2'>{item.name}</Text>
+            <Chip mode="flat" style={styles.chips}>
+              <Text type="body2">{item.name}</Text>
             </Chip>
           ))}
         </View>
       </View>
+      <View style={styles.btnContainer}>
+        <Button disabled={currentStep === 1} mode="text">
+          Back
+        </Button>
+        <Button onPress={() => handleNext()} mode="contained">
+          Next
+        </Button>
       </View>
+    </View>
   );
 };
 
