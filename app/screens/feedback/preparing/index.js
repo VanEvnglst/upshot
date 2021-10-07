138af7748c4cf236fd68d58de60e3763d93c08bd
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-07 09:24:04 +0800

    Update preparing step counter

--- app/screens/feedback/preparing/index.js
+++ app/screens/feedback/preparing/index.js
@@ -7,6 +7,8 @@ import { Wrapper, ProgressIndicator, Header, Text } from '../../../components';
 import PreparingStep1 from './step1';
 import PreparingStep2 from './step2';
 import PreparingStep3 from './step3';
+import PreparingStep3B from './step3B';
+import PreparingStep3C from './step3C';
 import PreparingStep4 from './step4';
 import PreparingStep5 from './step5';
 import labels from '../../../locales/en';
@@ -15,7 +17,7 @@ import styles from './styles';
 const FeedbackPreparing = props => {
   const { navigation } = props;
   const dispatch = useDispatch();
-  const [currentStep, setCurrentStep] = useState(1);
+  const [currentStep, setCurrentStep] = useState(5);
 
   const handleStepContent = () => {
     switch (currentStep) {
@@ -26,9 +28,21 @@ const FeedbackPreparing = props => {
       case 3:
         return <PreparingStep3 />;
       case 4:
-        return <PreparingStep4 />;
+        return <PreparingStep3B />;
       case 5:
+        return <PreparingStep3C />;
+      case 6:
+        return <PreparingStep4 />;
+      case 7:
+        return <PreparingStep4B />;
+      case 8:
+        return <PreparingStep4C />;
+      case 9:
         return <PreparingStep5 />;
+      case 10:
+        return <Preparing5B />;
+      case 11:
+        return <Preparing5C />;
     }
   };
 
@@ -43,8 +57,8 @@ const FeedbackPreparing = props => {
       <ProgressIndicator steps={5} currentIndex={currentStep} />
       <View style={styles.contentContainer}>{handleStepContent()}</View>
       <View style={styles.btnContainer}>
-        <Button mode='text'>Back</Button>
-        <Button mode='contained'>Next</Button>
+        <Button mode="text">Back</Button>
+        <Button mode="contained">Next</Button>
       </View>
     </Wrapper>
   );
