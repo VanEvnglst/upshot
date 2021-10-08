6f6fd0eca04b84bf1ede379f81fae93c89ac5765
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-08 17:12:10 +0800

    Create preparing step 5 screens

--- app/screens/feedback/preparing/step5/index.js
+++ app/screens/feedback/preparing/step5/index.js
@@ -1,9 +1,47 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { Wrapper } from '../../../../components';
+import { View, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
+import { Text, ButtonSelection } from 'app/components';
+import labels from 'app/locales/en';
+import styles from '../styles';
 
 const PreparingStep5 = props => {
-  return <Wrapper></Wrapper>;
+  const { checkOut } = labels.feedbackPreparing;
+  const dispatch = useDispatch();
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <KeyboardAvoidingView>
+        <View style={styles.descriptionContainer}>
+          <Text type="h6" style={styles.stepTitleText}>
+            {checkOut.step}: {checkOut.title}
+          </Text>
+          <Text style="body1" style={styles.stepDescriptionText}>
+            {checkOut.content}
+          </Text>
+        </View>
+        <ButtonSelection
+          type={'Check'}
+          title={checkOut.checkoutFeelings}
+          selected={false}
+        />
+        <ButtonSelection
+          type={'Check'}
+          title={checkOut.checkoutComfortable}
+          selected={false}
+        />
+        <ButtonSelection
+          type={'Check'}
+          title={checkOut.anythingElse}
+          selected={false}
+        />
+        <TextInput
+          placeholder={labels.common.inputHint}
+        />
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
 };
 
 export default PreparingStep5;
---
+++ app/screens/feedback/preparing/step5C/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { View, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
+import { Text, ButtonSelection } from 'app/components';
+import labels from 'app/locales/en';
+import styles from '../styles';
+
+const PreparingStep5C = props => {
+  const { checkOut } = labels.feedbackPreparing;
+  const dispatch = useDispatch();
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <KeyboardAvoidingView>
+        <View style={styles.descriptionContainer}>
+          <Text type="h6" style={styles.stepTitleText}>
+            {checkOut.step}: {checkOut.title}
+          </Text>
+          <Text style="body1" style={styles.stepDescriptionText}>
+            {checkOut.checkoutDate}
+          </Text>
+        </View>
+        <ButtonSelection
+          type={'Check'}
+          title={checkOut.checkoutTouchbase}
+        />
+        <TextInput
+          placeholder={labels.common.inputHint}
+        />
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
+};
+
+export default PreparingStep5C;
