5f1650b1ce973db9fb9097b423793b82c8ba4d1f
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-08 17:10:54 +0800

    Adjust preparing step 3

--- app/screens/feedback/preparing/step3/index.js
+++ app/screens/feedback/preparing/step3/index.js
@@ -1,27 +1,64 @@
 import React, { useState } from 'react';
 import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
-import { Wrapper, Text } from '../../../../components';
-import labels from '../../../../locales/en';
+import { Wrapper, Text } from 'app/components';
+import labels from 'app/locales/en';
+import styles from '../styles';
 
 const PreparingStep3 = props => {
   const { describeDiscuss } = labels.feedbackPreparing;
+  const [isEventFocused, setEventFocus] = useState(false);
+  const [isActionFocused, setActionFocus] = useState(false);
+  const [isResultFocused, setResultFocus] = useState(false);
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <KeyboardAvoidingView>
-        <View>
-          <View>
-            <Text type="h6">
-              {describeDiscuss.step}: {describeDiscuss.title}
-            </Text>
-            <Text type="body1">{describeDiscuss.describeEvent}</Text>
-            <TextInput placeholder={describeDiscuss.describeEventHint} />
-            <Text type="body1">{describeDiscuss.describeAction}</Text>
-            <TextInput placeholder={describeDiscuss.describeActionHint} />
-            <Text type="body1">{describeDiscuss.describeImpact}</Text>
-            <TextInput placeholder={describeDiscuss.describeImpactHint} />
-          </View>
+        <View style={styles.descriptionContainer}>
+          <Text type="h6" style={styles.stepTitleText}>
+            {describeDiscuss.step}: {describeDiscuss.title}
+          </Text>
+          <Text type="body1" style={styles.stepDescriptionText}>
+            {describeDiscuss.describeEvent}
+          </Text>
         </View>
+        <TextInput
+          type="flat"
+          label={isEventFocused ? describeDiscuss.describeEventHint : null}
+          placeholder={
+            isEventFocused ? null : describeDiscuss.describeEventHint
+          }
+          onBlur={() => setEventFocus(false)}
+          onFocus={() => setEventFocus(true)}
+        />
+        <View style={styles.descriptionContainer}>
+          <Text type="body1" style={styles.stepDescriptionText}>
+            {describeDiscuss.describeAction}
+          </Text>
+        </View>
+        <TextInput
+          type="flat"
+          label={isActionFocused ? describeDiscuss.describeActionHint : null}
+          placeholder={
+            isActionFocused ? null : describeDiscuss.describeActionHint
+          }
+          onBlur={() => setActionFocus(false)}
+          onFocus={() => setActionFocus(true)}
+        />
+        <View style={styles.descriptionContainer}>
+          <Text type="body1" style={styles.stepDescriptionText}>
+            {describeDiscuss.describeImpact}
+          </Text>
+        </View>
+        <TextInput
+          type="flat"
+          label={isResultFocused ? describeDiscuss.describeResultHint : null}
+          placeholder={
+            isResultFocused ? null : describeDiscuss.describeImpactHint
+          }
+          onBlur={() => setResultFocus(false)}
+          onFocus={() => setResultFocus(true)}
+        />
       </KeyboardAvoidingView>
     </ScrollView>
   );
