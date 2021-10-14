993434df2499a0a1ef7a8cf7ae1c9474e1cb542c
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-14 19:47:37 +0800

    Add signed in to selectors list

--- app/store/selectors.js
+++ app/store/selectors.js
@@ -1,11 +1,15 @@
 /* ------------- User Selectors ------------- */
 export const getUserFirstName = state => state.user.get('firstName');
 
+/* ------------- Auth Selectors ------------- */
+export const getSignInState = state => state.authentication.get('isSignedIn');
+
 /* ------------- Feedback Selectors ------------- */
 export const getCurrentJourney = state => state.feedback.get('currentJourney');
 export const getFeedbackFlowList = state =>
   state.feedback.get('feedbackFlow').data;
 export const getFeedbackTypeList = state =>
   state.feedback.get('feedbackType').data;
-export const getRelatedTopicsList = state => state.feedbaback.get('relatedTopics').data;
+export const getRelatedTopicsList = state =>
+  state.feedbaback.get('relatedTopics').data;
 export const getChosenFlow = state => state.feedback.get('chosenFlow');
