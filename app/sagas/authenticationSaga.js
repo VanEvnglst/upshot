dd9bec44790811113f718ed47dcb86c0099de0cf
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-14 19:45:06 +0800

    Integrate sign in API

--- app/sagas/authenticationSaga.js
+++ app/sagas/authenticationSaga.js
@@ -2,19 +2,36 @@ import { checkInternetConnection } from 'react-native-offline';
 import { call, put, takeLatest, select } from 'redux-saga/effects';
 import AuthenticationActions, {
   AuthenticationTypes,
-} from '../store/authenticationRedux';
+} from 'app/store/authenticationRedux';
+import api from 'app/services/apiService';
 
-export function* signInUser() {
+export function* signInUser({ email, password }) {
   const connected = yield checkInternetConnection();
-  if (!connected) {
-    //yield showNetworkError();
-    return;
-  }
-  const authResponse = yield call(api.signInUser, { email, password });
+  // if (!connected) {
+  //   //yield showNetworkError();
+  //   return;
+  // }
+  const authResponse = yield call(api.signIn, { email, password });
   if (authResponse.ok) {
-    console.log('auth ok');
+    console.log('auth ok', authResponse.data);
   }
 }
+
+// export function* signUpUser({ email, password, firstName, lastName }) {
+//   const connected = yield checkInternetConnection();
+
+//   const response = yield call(api.signUp, {
+//     email,
+//     password,
+//     firstName,
+//     lastName,
+//     token,
+//     role
+//   });
+//   if (response.ok) {
+//     console.log('res', response);
+//   }
+// }
 
 function* watchAuthenticationSaga() {
   yield takeLatest(AuthenticationTypes.SIGN_IN_USER, signInUser);
