30ca3bdf31222445555562e2285ec9fc319d637e
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-15 16:38:35 +0800

    integrate fetch team members api

--- app/screens/feedback/documenting/step1/index.js
+++ app/screens/feedback/documenting/step1/index.js
@@ -1,45 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { Chip, Button } from 'react-native-paper';
+
 import { Text } from 'app/components';
-import FeedbackActions from '../../../../store/feedback/feedbackRedux';
+import FeedbackActions from 'app/store/feedback/feedbackRedux';
 import DocumentingActions from 'app/store/feedback/documentingRedux';
+import { getStaffList } from 'app/store/selectors';
 import labels from 'app/locales/en';
 import styles from './styles';
 
 const DocumentingStep1 = props => {
   const dispatch = useDispatch();
+  const currentStep = useSelector(state => state.documenting.get('activeStep'));
+  const staffList = useSelector(getStaffList);
+  const isLoading = useSelector(
+    state => state.feedback.get('teamMembers').fetching,
+  );
+  const [teamMember, setTeamMember] = useState();
 
-  //TODO: Fix selector
-  // const currentStep = useSelector(state => state.documenting.get('activeStep'r));
+  const chooseTeamMember = name => {
+    debugger;
+    setTeamMember(name);
+  };
 
   useEffect(() => {
-    // dispatch(FeedbackActions.fetchTeamMembers)
+    dispatch(FeedbackActions.fetchTeamMembers());
   }, []);
 
   const handleNext = () => {
-    console.log('1231241', currentStep);
-    dispatch(DocumentingActions.setActiveStep(currentStep));
+    //dispatch(DocumentingActions.setActiveStep(currentStep));
   };
 
-  const names = [
-    {
-      name: 'Teammate',
-    },
-    {
-      name: 'Teammate',
-    },
-    {
-      name: 'Teammate',
-    },
-    {
-      name: 'Teammate',
-    },
-    {
-      name: 'Teammate',
-    },
-  ];
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
@@ -48,11 +40,17 @@ const DocumentingStep1 = props => {
         </Text>
 
         <View style={styles.namesContainer}>
-          {names.map((item, i) => (
-            <Chip mode="flat" style={styles.chips}>
-              <Text type="body2">{item.name}</Text>
-            </Chip>
-          ))}
+          {isLoading && <ActivityIndicator />}
+          {staffList &&
+            staffList.map((item, i) => (
+              <Chip
+                key={i}
+                onPress={val => chooseTeamMember(val)}
+                mode="flat"
+                style={styles.chips}>
+                <Text type="body2">{item}</Text>
+              </Chip>
+            ))}
         </View>
       </View>
       <View style={styles.btnContainer}>
