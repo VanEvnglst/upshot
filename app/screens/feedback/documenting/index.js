e38c7be46faee708852ca0c12096872b05c9b832
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-18 17:52:33 +0800

    integrate documenting API

--- app/screens/feedback/documenting/index.js
+++ app/screens/feedback/documenting/index.js
@@ -1,21 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { View, ScrollView } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, ProgressBar } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { Wrapper, ProgressIndicator, Header, Text } from 'app/components';
+import {
+  getDocumentingStep,
+  getDocumentingMaxSteps,
+} from 'app/store/selectors';
+import DocumentingActions from 'app/store/feedback/documentingRedux';
 import DocumentingStep4 from './step4';
 import DocumentingStep3 from './step3';
 import DocumentingStep2 from './step2';
 import DocumentingStep1 from './step1';
+import Colors from 'app/theme/colors';
 import styles from './styles';
 
 const FeedbackDocumenting = () => {
   const dispatch = useDispatch();
-  const [currentStep, setCurrentStep] = useState(1);
+  const activeStep = useSelector(getDocumentingStep);
+  const maxStep = useSelector(getDocumentingMaxSteps);
+  const indexValue = activeStep / maxStep;
 
+  useEffect(() => {
+    console.log('step', activeStep);
+  });
   const handleStepContent = () => {
-    switch (currentStep) {
+    switch (1) {
       case 1:
         return <DocumentingStep1 />;
       case 2:
@@ -27,15 +38,25 @@ const FeedbackDocumenting = () => {
     }
   };
 
+  const handleCloseBtn = () => {
+    if (activeStep === 1) {
+      dispatch(DocumentingActions.resetActiveStep());
+    }
+  };
+
   return (
     <Wrapper>
       <Header
         headerRight={{
-          onPress: () => console.log('Gawa ka bottom sheet'),
+          onPress: () => handleCloseBtn(),
         }}
       />
       <Text type="overline">Documenting</Text>
-      <ProgressIndicator steps={4} currentIndex={currentStep} />
+      <ProgressBar
+        progress={indexValue}
+        color={Colors.secondary}
+        style={styles.progressBar}
+      />
       <View style={styles.contentContainer}>{handleStepContent()}</View>
     </Wrapper>
   );
