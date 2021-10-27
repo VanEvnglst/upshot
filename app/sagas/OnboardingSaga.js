d47a982daa76fb45bcbf7758f78aa15eef9bfb3f
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-27 17:50:49 +0800

    handle onboarding api integration

--- app/sagas/OnboardingSaga.js
+++ app/sagas/OnboardingSaga.js
@@ -1,15 +1,28 @@
 import { checkInternetConnection } from 'react-native-offline';
 import { call, put, takeLatest, select } from 'redux-saga/effects';
 import OnboardingActions, { OnboardingTypes } from 'app/store/OnboardingRedux';
+import AuthenticationActions from 'app/store/authenticationRedux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import api from 'app/services/apiService';
 
+const RESULT_SUCCESS = 'success';
 export function* signUpUser({ data }) {
-  const { email, password, firstName, lastName, token, role } = data;
-  const connected = yield checkInternetConnection();
+  //const connected = yield checkInternetConnection();
 
   const response = yield call(api.signUp, data);
+  debugger;
   if (response.ok) {
-    console.log('res', response.data);
+    if (response.data.result === RESULT_SUCCESS) {
+      AsyncStorage.setItem('uniqueId', response.data.user.user_uuid);
+      yield put(OnboardingActions.signUpUserSuccess());
+      yield put(AuthenticationActions.setUserSignedIn());
+    } else {
+      console.log('register error', response.data);
+      OnboardingActions.signUpUserFailure(response.data);
+    }
+  } else {
+    OnboardingActions.signUpUserFailure();
   }
 }
 
