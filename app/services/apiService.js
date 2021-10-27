67172b005495db93d980b8b1948e9796eb3bcd91
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-10-27 17:53:09 +0800

    Update api implementation

--- app/services/apiService.js
+++ app/services/apiService.js
@@ -3,25 +3,25 @@ import { upshotAPI } from '../config/apiConfig';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default {
-  signIn: async ({ email, password }) => {
-    const payload = { email, passwd: password };
+  signIn: async payload => {
+    const params = new URLSearchParams();
+    params.append('email', payload.email);
+    params.append('passwd', payload.password);
     debugger;
-    return upshotAPI.post('/login', payload);
+    return upshotAPI.post('/login', params);
   },
 
   signUp: async data => {
-    const { email, password, firstName, lastName, role, token } = data;
-    const payload = {
-      email,
-      passwd: password,
-      firstname: firstName,
-      lastname: lastName,
-      role,
-      token,
-    };
-    console.log('data', payload);
-    debugger;
-    return upshotAPI.post('/signup', payload);
+    const { email, password, firstName, lastName, token, role } = data;
+    const params = new URLSearchParams();
+    params.append('email', email);
+    params.append('passwd', password);
+    params.append('firstname', firstName);
+    params.append('lastname', lastName);
+    params.append('token', token);
+    params.append('role', role);
+
+    return upshotAPI.post('/signup', params);
   },
 
   getFeedbackType: async () => {
@@ -45,21 +45,23 @@ export default {
     return upshotAPI.get('/trivias');
   },
   postNewJourney: async data => {
+    const params = new URLSearchParams();
+    params.append('feedback-type',data);
     const uniqueId = await AsyncStorage.getItem('uniqueId');
-    const { feedbackTypeId } = data;
-    const payload = {
-      'feedback-type': feedbackTypeId
-    }
-    
-    return upshotAPI.post(`/${uniqueId}/journey/new`, payload);
+
+    return upshotAPI.post(`/${uniqueId}/journey/new`, params);
   },
+
   postFeedbackDocumenting: async data => {
     const uniqueId = await AsyncStorage.getItem('uniqueId');
+    const params = new URLSearchParams();
     const {} = data;
-    const payload = {
-
-    }
-    return upshotAPI.post(`/${uniqueId}/feedback/documenting`, payload);
+    params.append('journey_id', data)
+    params.append('staff_id')
+    params.append('topic_id')
+    params.append('incident_date')
+    params.append('reminder')
+    return upshotAPI.post(`/${uniqueId}/feedback/documenting`, params);
   },
   updateDocumenting: async () => {},
 };
