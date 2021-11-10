ea3dae399827eb6ece917578dec0dfd976a0fe6f
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-11-10 21:37:10 +0800

    Update documenting step 2 screen

--- app/screens/feedback/documenting/step2/index.js
+++ app/screens/feedback/documenting/step2/index.js
@@ -4,13 +4,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-native-paper';
 import { HintIndicator, ButtonSelection, Text } from 'app/components';
 import DocumentingActions from 'app/store/feedback/documentingRedux';
-import { getFeedbackTypeList, getDocumentingStep } from 'app/store/selectors';
-import FeedbackActions from 'app/store/feedback/feedbackRedux';
+import {
+  getFeedbackTypeList,
+  getDocumentingStep,
+  getStep2Data,
+} from 'app/store/selectors';
 import labels from 'app/locales/en';
 import containerStyles from '../styles';
 
 const DocumentingStep2 = props => {
   const dispatch = useDispatch();
+  const stepData = useSelector(getStep2Data);
   const feedbackTypes = useSelector(getFeedbackTypeList);
   const activeStep = useSelector(getDocumentingStep);
   const [isCompleted, setCompletion] = useState(false);
@@ -19,6 +23,10 @@ const DocumentingStep2 = props => {
     type: '',
   });
   const [hint, showHint] = useState(false);
+
+  useEffect(() => {
+    if (stepData.data) _handleFeedbackType(stepData.data);
+  }, [stepData]);
 
   const _handleFeedbackType = item => {
     setFeedbackType(item);
@@ -36,25 +44,25 @@ const DocumentingStep2 = props => {
   return (
     <View style={containerStyles.container}>
       <View style={containerStyles.contentContainer}>
-      <Text type="h6" style={containerStyles.stepTitleText}>
-        {labels.feedbackDocumenting.feedbackToGive}
-      </Text>
-      {feedbackTypes.map((item, i) => (
-        <ButtonSelection
-          title={item.display_name}
-          type={'Radio'}
-          content={item.hint}
-          showHint={hint}
-          onPress={() => _handleFeedbackType(item)}
-          selected={item.id === feedbackType.id}
-          key={item.id}
-        />
-      ))}
-      <HintIndicator showHint={hint} onPress={() => showHint(!hint)} />
+        <Text type="h6" style={containerStyles.stepTitleText}>
+          {labels.feedbackDocumenting.feedbackToGive}
+        </Text>
+        {feedbackTypes.map((item, i) => (
+          <ButtonSelection
+            title={item.display_name}
+            type={'Radio'}
+            content={item.hint}
+            showHint={hint}
+            onPress={() => _handleFeedbackType(item)}
+            selected={item.id === feedbackType.id}
+            key={item.id}
+          />
+        ))}
+        <HintIndicator showHint={hint} onPress={() => showHint(!hint)} />
       </View>
       <View style={containerStyles.btnContainer}>
         <Button mode="text" onPress={() => handleBack()}>
-        {labels.common.back}
+          {labels.common.back}
         </Button>
         <Button
           style={styles.button}
