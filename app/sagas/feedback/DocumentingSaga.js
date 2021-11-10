eca42decdd50b73569910ddd5bc6fbd214a2d740
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-11-10 21:34:14 +0800

    Integrate documenting saga

---
+++ app/sagas/feedback/DocumentingSaga.js
@@ -0,0 +1,39 @@
+import { checkInternetConnection } from 'react-native-offline';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
+import DocumentingActions, {
+  DocumentingTypes,
+} from 'app/store/feedback/documentingRedux';
+import api from 'app/services/apiService';
+
+const activeStep = state => state.documenting.get('activeStep');
+
+export function* postFeedbackDocumenting({ data }) {
+  // const connected = yield checkInternetConnection();
+  // if (!connected) {
+  //  return;
+  //}
+  const response = yield call(api.postFeedbackDocumenting, data);
+  debugger;
+  if (response.ok) {
+    if (response.data.status === 'ok') {
+      const step = yield select(activeStep);
+      const documentingId = response.data.details.id;
+      yield put(
+        DocumentingActions.postFeedbackDocumentingSuccess(documentingId),
+      );
+      debugger;
+      yield put(DocumentingActions.setActiveStep(step + 1));
+    }
+  } else {
+    yield put(DocumentingActions.postFeedbackDocumentingFailure(response.data));
+  }
+}
+
+function* watchDocumentingSaga() {
+  yield takeLatest(
+    DocumentingTypes.POST_FEEDBACK_DOCUMENTING,
+    postFeedbackDocumenting,
+  );
+}
+
+export default watchDocumentingSaga;
