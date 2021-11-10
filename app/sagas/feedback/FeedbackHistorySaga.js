7ce60291b244454a861526fdbe5d0114dde2115c
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-11-10 21:33:56 +0800

    Integrate feedback history saga

---
+++ app/sagas/feedback/FeedbackHistorySaga.js
@@ -0,0 +1,51 @@
+import { checkInternetConnection } from 'react-native-offline';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
+import FeedbackHistoryActions, {
+  FeedbackHistoryTypes,
+} from 'app/store/feedback/feedbackHistoryRedux';
+import api from 'app/services/apiService';
+
+const STATUS_OK = 'ok';
+
+export function* fetchActiveJourneys() {
+  const response = yield call(api.getOpenFeedbackJourneys);
+  if (response.ok) {
+    if (response.data.status === STATUS_OK) {
+      const journeyList = response.data.details;
+      yield put(FeedbackHistoryActions.fetchActiveJourneysSuccess(journeyList));
+    } else {
+      yield put(FeedbackHistoryActions.fetchActiveJourneysFailure());
+    }
+  } else {
+    yield put(FeedbackHistoryActions.fetchActiveJourneysFailure());
+  }
+}
+
+export function* fetchRecentJourneys() {
+  const response = yield call(api.getClosedFeedbackJourneys);
+  if (response.ok) {
+    if (response.data.status === STATUS_OK) {
+      const recentList = response.data.details;
+      yield put(FeedbackHistoryActions.fetchRecentJourneysSuccess(recentList));
+    } else {
+      yield put(
+        FeedbackHistoryActions.fetchRecentJourneysFailure(response.data),
+      );
+    }
+  } else {
+    yield put(FeedbackHistoryActions.fetchRecentJourneysFailure());
+  }
+}
+
+function* watchFeedbackHistorySaga() {
+  yield takeLatest(
+    FeedbackHistoryTypes.FETCH_ACTIVE_JOURNEYS,
+    fetchActiveJourneys,
+  );
+  yield takeLatest(
+    FeedbackHistoryTypes.FETCH_RECENT_JOURNEYS,
+    fetchRecentJourneys,
+  );
+}
+
+export default watchFeedbackHistorySaga;
