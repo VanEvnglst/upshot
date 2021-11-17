96886092bd10c341761e4449b5591093f498faef
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-11-17 16:12:26 +0800

    Provide prop types for hint indicator component

--- app/components/hint-indicator/index.js
+++ app/components/hint-indicator/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from './styles';
 
@@ -9,16 +10,24 @@ const HintIndicator = props => {
     <TouchableOpacity
       accessibilityRole="button"
       onPress={onPress}
-      style={[styles.container,
-      showHint && styles.showHintContainer]}>
+      style={[styles.container, showHint && styles.showHintContainer]}>
       <Icon
         name={'help-outline'}
         size={18}
-        style={[styles.iconStyle,
-        showHint && styles.showHintIcon]}
+        style={[styles.iconStyle, showHint && styles.showHintIcon]}
       />
     </TouchableOpacity>
   );
 };
 
 export default HintIndicator;
+
+HintIndicator.PropTypes = {
+  showHint: PropTypes.bool.isRequired,
+  onPress: PropTypes.func.isRequired,
+};
+
+HintIndicator.defaultProps = {
+  showHint: false,
+  onPress: () => {},
+};
