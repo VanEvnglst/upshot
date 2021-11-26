be73f3f0097cd83e29fed18bee308433043969db
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-11-26 19:40:52 +0800

    Update preparing redux

--- app/store/feedback/preparingRedux.js
+++ app/store/feedback/preparingRedux.js
@@ -23,20 +23,44 @@ export const PREPARING_STATE = Map({
 /* ------------- Types and Action Creators ------------- */
 const { Types, Creators } = createActions({
   setPrepActiveStep: ['data'],
+  setPreparingData: ['key', 'data'],
+  setPreparingStatus: ['key', 'data'],
   postPreparing: [''],
   postPreparingSuccess: null,
   postPreparingFailure: ['error'],
   updatePreparing: [''],
+  resetPreparingState: null,
 });
 
 export const PreparingTypes = Types;
 export default Creators;
 
 /* ------------- Reducers ------------- */
-const setPrepActiveStep = (state, { step }) =>
-  state.merge({ activeStep: step });
+const setPrepActiveStep = (state, { step }) => {
+  if (state.get('activeStep' >= state.get('maxStep'))) {
+    return;
+  }
+  return state.merge({ activeStep: step });
+}
+
+const resetPreparingState = state => state.merge(INITIAL_STATE);
+
+const setPreparingData = (state, { key, data}) => {
+  return state.merge({
+    [key]: { data },
+  });
+};
+
+const setPreparingStatus = (state, { key, data }) => {
+  return state.merge({
+    [key]: data
+  });
+}
 
 /* ------------- Hookup Reducers To Types ------------- */
 export const reducer = createReducer(PREPARING_STATE, {
   [Types.SET_PREP_ACTIVE_STEP]: setPrepActiveStep,
+  [Types.SET_PREPARING_DATA]: setPreparingData,
+  [Types.SET_PREPARING_STATUS]: setPreparingStatus,
+  [Types.RESET_PREPARING_STATE]: resetPreparingState,
 });
