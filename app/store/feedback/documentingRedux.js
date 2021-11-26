a2a9b09786afea1fc963efc62fa1d30a18b4676e
Ivan Evangelista <ivanevangelista@Ivans-MacBook-Pro.local>
2021-11-26 19:35:41 +0800

    Update documenting redux

--- app/store/feedback/documentingRedux.js
+++ app/store/feedback/documentingRedux.js
@@ -15,6 +15,8 @@ export const INITIAL_STATE = Map({
   step3: { ...defaultState },
   step4: { ...defaultState },
   reminderTime: null,
+  closed: false,
+  started: false,
 });
 
 /* ------------- Types and Action Creators ------------- */
@@ -22,10 +24,13 @@ const { Types, Creators } = createActions({
   postFeedbackDocumenting: ['data'],
   postFeedbackDocumentingSuccess: ['documentingId'],
   postFeedbackDocumentingFailure: ['error'],
-  updateFeedbackDocumenting: [''],
+  updateFeedbackDocumenting: ['data'],
+  updateFeedbackDocumentingSuccess: null,
+  updateFeedbackDocumenntingFailure: ['error'],
   setActiveStep: ['step'],
   resetDocumentingState: null,
   setDocumentingData: ['key', 'data'],
+  setDocumentingStatus: ['key', 'status'],
 });
 
 /* ------------- Reducers ------------- */
@@ -33,20 +38,24 @@ export const DocumentingTypes = Types;
 export default Creators;
 
 const setActiveStep = (state, { step }) => {
-  console.log('stat', state);
   if (state.get('activeStep' >= state.get('maxStep'))) {
     return;
   }
   return state.merge({ activeStep: step });
 };
 
-const resetDocumentingState = state =>
-  state.merge(INITIAL_STATE);
+const resetDocumentingState = state => state.merge(INITIAL_STATE);
 
 const setDocumentingData = (state, { key, data }) => {
   const stepData = state.get(key);
   return state.merge({
     [key]: { data },
+  });
+};
+
+const setDocumentingStatus = (state, { key, status }) => {
+  return state.merge({
+    [key]: status,
   });
 };
 
@@ -56,7 +65,6 @@ const postFeedbackDocumenting = state =>
   });
 
 const postFeedbackDocumentingSuccess = (state, { documentingId }) => {
-  debugger;
   return state.merge({
     id: documentingId,
   });
@@ -64,15 +72,33 @@ const postFeedbackDocumentingSuccess = (state, { documentingId }) => {
 
 const postFeedbackDocumentingFailure = (state, { error }) => state.merge({});
 
-const updateFeedbackDocumenting = state => state.merge({});
+const updateFeedbackDocumenting = state =>
+  state.merge({
+    fetching: true,
+    error: '',
+  });
+
+const updateFeedbackDocumentingSuccess = state =>
+  state.merge({
+    fetching: false,
+  });
+
+const updateFeedbackDocumentingFailure = state =>
+  state.merge({
+    fetching: false,
+  });
 
 /* ------------- Hookup Reducers To Types ------------- */
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.SET_ACTIVE_STEP]: setActiveStep,
   [Types.RESET_DOCUMENTING_STATE]: resetDocumentingState,
   [Types.SET_DOCUMENTING_DATA]: setDocumentingData,
+  [Types.SET_DOCUMENTING_STATUS]: setDocumentingStatus,
   [Types.POST_FEEDBACK_DOCUMENTING]: postFeedbackDocumenting,
   [Types.POST_FEEDBACK_DOCUMENTING_SUCCESS]: postFeedbackDocumentingSuccess,
   [Types.POST_FEEDBACK_DOCUMENTING_FAILURE]: postFeedbackDocumentingFailure,
   [Types.UPDATE_FEEDBACK_DOCUMENTING]: updateFeedbackDocumenting,
+  [Types.UPDATE_FEEDBACK_DOCUMENTING_SUCCESS]: updateFeedbackDocumentingSuccess,
+
+  // [Types.UPDATE_FEEDBACK_DOCUMENTING_FAILURE]: updateFeedbackDocumentingFailure,
 });
