484df42ba45a9c2548fd533bcd7331554923446b
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-03 18:18:29 +0800

    create new data model

---
+++ app/models/PreparingObservations.js
@@ -0,0 +1,17 @@
+import labels from 'app/locales/en';
+
+const { describeDiscuss } =
+  labels.feedbackPreparing;
+
+const preparingObservations = [
+  {
+    id: 1,
+    title: describeDiscuss.observeOption1
+  },
+  {
+    id: 2,
+    title: describeDiscuss.observeOption2
+  },
+];
+
+export default preparingObservations;
