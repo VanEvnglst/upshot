f34d9373a2ca66bb5b9c15625fc83fac18122844
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-03 18:19:15 +0800

    Update preapring step 3

--- app/screens/feedback/preparing/step3/index.js
+++ app/screens/feedback/preparing/step3/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import PreparingActions from 'app/store/feedback/preparingRedux';
-import { getPreparingStep } from 'app/store/selectors';  
-import { Text } from 'app/components';
+import { getPreparingStep } from 'app/store/selectors';
+import { Text, TextInput } from 'app/components';
 import labels from 'app/locales/en';
 import containerStyles from '../styles';
 
@@ -13,17 +13,33 @@ const PreparingStep3 = props => {
   const { describeDiscuss } = labels.feedbackPreparing;
   const dispatch = useDispatch();
   const activeStep = useSelector(getPreparingStep);
-  const [isEventFocused, setEventFocus] = useState(false);
-  const [isActionFocused, setActionFocus] = useState(false);
-  const [isResultFocused, setResultFocus] = useState(false);
+  //TODO: useSeletor for stepData
+  const [eventDescription, setEventDescription] = useState();
+  const [actionDescription, setActionDescription] = useState();
+  const [resultDescription, setResultDescription] = useState();
   const [isCompleted, setCompletion] = useState(false);
 
+  const handleDescriptionText = (key, text) => {
+    switch (key) {
+      case 'event':
+        setEventDescription(text);
+        break;
+      case 'action':
+        setActionDescription(text);
+        break;
+      case 'result':
+        setResultDescription(text);
+    }
+    if (eventDescription && actionDescription && resultDescription)
+      setCompletion(true);
+  };
 
   const handleBack = () => {
     dispatch(PreparingActions.setPrepActiveStep(activeStep - 1));
   };
 
   const handleNext = () => {
+    //TODO: add dispatch for saving data
     dispatch(PreparingActions.setPrepActiveStep(activeStep + 1));
   };
 
@@ -40,12 +56,10 @@ const PreparingStep3 = props => {
         </View>
         <TextInput
           type="flat"
-          label={isEventFocused ? describeDiscuss.describeEventHint : null}
-          placeholder={
-            isEventFocused ? null : describeDiscuss.describeEventHint
-          }
-          onBlur={() => setEventFocus(false)}
-          onFocus={() => setEventFocus(true)}
+          label={describeDiscuss.describeEventHint}
+          placeholder={describeDiscuss.describeEventHint}
+          value={eventDescription}
+          onChangeText={text => handleDescriptionText('event', text)}
         />
         <View style={containerStyles.descriptionContainer}>
           <Text type="body1" style={containerStyles.stepDescriptionText}>
@@ -54,12 +68,10 @@ const PreparingStep3 = props => {
         </View>
         <TextInput
           type="flat"
-          label={isActionFocused ? describeDiscuss.describeActionHint : null}
-          placeholder={
-            isActionFocused ? null : describeDiscuss.describeActionHint
-          }
-          onBlur={() => setActionFocus(false)}
-          onFocus={() => setActionFocus(true)}
+          label={describeDiscuss.describeActionHint}
+          placeholder={describeDiscuss.describeActionHint}
+          value={actionDescription}
+          onChangeText={text => handleDescriptionText('action', text)}
         />
         <View style={containerStyles.descriptionContainer}>
           <Text type="body1" style={containerStyles.stepDescriptionText}>
@@ -68,27 +80,24 @@ const PreparingStep3 = props => {
         </View>
         <TextInput
           type="flat"
-          label={isResultFocused ? describeDiscuss.describeResultHint : null}
-          placeholder={
-            isResultFocused ? null : describeDiscuss.describeImpactHint
-          }
-          onBlur={() => setResultFocus(false)}
-          onFocus={() => setResultFocus(true)}
+          label={describeDiscuss.describeResultHint}
+          placeholder={describeDiscuss.describeImpactHint}
+          value={resultDescription}
+          onChangeText={text => handleDescriptionText('result', text)}
         />
       </KeyboardAvoidingView>
       <View style={containerStyles.btnContainer}>
         <Button
-          mode='text'
+          mode="text"
           onPress={() => handleBack()}
-          testID={'btn-preparingStep3-back'}
-        >
+          testID={'btn-preparingStep3-back'}>
           {labels.common.back}
         </Button>
         <Button
+          disabled={!isCompleted}
           onPress={() => handleNext()}
           mode={isCompleted ? 'contained' : 'text'}
-          testID={'btn-preparingStep3-next'}
-        >
+          testID={'btn-preparingStep3-next'}>
           {labels.common.next}
         </Button>
       </View>
@@ -98,6 +107,12 @@ const PreparingStep3 = props => {
 
 export default PreparingStep3;
 
-PreparingStep3.propTypes = {};
+PreparingStep3.propTypes = {
+  setPrepActiveStep: PropTypes.func,
+  activeStep: PropTypes.number,
+};
 
-PreparingStep3.defaultProps = {};
+PreparingStep3.defaultProps = {
+  setPrepActiveStep: () => {},
+  activeStep: 1
+};
