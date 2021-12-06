c57cb33b35846683e326c1591aff319f618e1f20
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-06 19:47:10 +0800

    add selectors to list

--- app/store/selectors.js
+++ app/store/selectors.js
@@ -32,5 +32,15 @@ export const getStep4Data = state => state.documenting.get('step4');
 
 
 /* ------------- Feedback Preparing Selectors ------------- */
+export const getPreparingLoading = state => state.preparing.get('fetching');
 export const getPreparingStep = state => state.preparing.get('activeStep');
 export const getPreparingMaxSteps = state => state.preparing.get('maxStep');
+export const getPrepStep1Data = state => state.preparing.get('step1');
+export const getPrepStep2Data = state => state.preparing.get('step2');
+export const getPrepStep3Data = state => state.preparing.get('step3');
+export const getPrepStep3BData = state => state.preparing.get('step3B');
+export const getPrepStep4Data = state => state.preparing.get('step4');
+export const getPrepStep4BData = state => state.preparing.get('step4B');
+export const getPrepStep5Data = state => state.preparing.get('step5');
+export const getPrepStep5BData = state => state.preparing.get('step5B');
+export const getPrepStep5CData = state => state.preparing.get('step5C');
