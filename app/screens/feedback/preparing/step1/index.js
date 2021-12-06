900c22d4d02a9250c23fceebe99b840f6041fa19
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-06 17:27:18 +0800

    Update preparing step1 screen

--- app/screens/feedback/preparing/step1/index.js
+++ app/screens/feedback/preparing/step1/index.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import { View, KeyboardAvoidingView } from 'react-native';
+import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Button } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import PreparingActions from 'app/store/feedback/preparingRedux';
-import { getPreparingStep } from 'app/store/selectors';
+import { getPreparingStep, getPrepStep1Data } from 'app/store/selectors';
 import { Text, TextInput } from 'app/components';
 import labels from 'app/locales/en';
 import containerStyles from '../styles';
 import styles from './styles';
 
-const PreparingStep1 = props => {
+const PreparingStep1 = () => {
   const { checkIn } = labels.feedbackPreparing;
   const dispatch = useDispatch();
   const activeStep = useSelector(getPreparingStep);
-  //TODO: useSelector for stepData
+  const stepData = useSelector(getPrepStep1Data);
   const [checkInValue, setCheckInValue] = useState();
   const [isCompleted, setCompletion] = useState(false);
 
@@ -23,7 +23,9 @@ const PreparingStep1 = props => {
     if (checkInValue !== '') setCompletion(true);
   };
 
-  // TODO: add useEffect for stepData
+  useEffect(() => {
+    if (stepData.data) handleText(stepData.data)
+  },[stepData]);
 
   const handleNext = () => {
     // dispatch(PreparingActions.setPreparingData('step1', checkInValue));
@@ -32,28 +34,32 @@ const PreparingStep1 = props => {
 
   return (
     <View style={styles.container}>
-      <KeyboardAvoidingView>
-        <View style={containerStyles.descriptionContainer}>
-          <Text type="h6" style={containerStyles.stepTitleText}>
-            {checkIn.step}: {checkIn.title}
-          </Text>
-          <Text type="body1" style={containerStyles.stepDescriptionText}>
-            {checkIn.content}
-          </Text>
-        </View>
-        <View>
-          <TextInput
-            label={checkIn.checkInHint}
-            placeholder={checkIn.checkInHint}
-            value={checkInValue}
-            onChangeText={text => handleText(text)}
-            textAlignVertical="top"
-            multiline
-            numberOfLines={5}
-            theme={{ fonts: { regular: { fontFamily: 'Raleway-Regular' } } }}
-          />
-        </View>
-      </KeyboardAvoidingView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+      >
+        <KeyboardAvoidingView>
+          <View style={containerStyles.descriptionContainer}>
+            <Text type="h6" style={containerStyles.stepTitleText}>
+              {checkIn.step}: {checkIn.title}
+            </Text>
+            <Text type="body1" style={containerStyles.stepDescriptionText}>
+              {checkIn.content}
+            </Text>
+          </View>
+          <View>
+            <TextInput
+              label={checkIn.checkInHint}
+              placeholder={checkIn.checkInHint}
+              value={checkInValue}
+              onChangeText={text => handleText(text)}
+              textAlignVertical="top"
+              multiline
+              numberOfLines={5}
+              theme={{ fonts: { regular: { fontFamily: 'Raleway-Regular' } } }}
+            />
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
       <View style={styles.btnContainer}>
         <Button
           style={styles.button}
@@ -72,11 +78,14 @@ export default PreparingStep1;
 
 PreparingStep1.propTypes = {
   setPrepActiveStep: PropTypes.func,
+  setPreparingData: PropTypes.func,
   activeStep: PropTypes.number,
+  stepData: PropTypes.object,
 };
-
 
 PreparingStep1.defaultProps = {
   setPrepActiveStep: () => {},
+  setPreparingData: () => {},
   activeStep: 1,
-}
+  stepData: {},
+};
