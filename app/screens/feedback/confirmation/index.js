8d5142ce607ba89d9f0ebef3afc65acabbf8e2e3
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-09 20:23:28 +0800

    update feedback confirmation screen

--- app/screens/feedback/confirmation/index.js
+++ app/screens/feedback/confirmation/index.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image } from 'react-native';
-import { Button } from 'react-native-paper';
-import { Text, Wrapper, Header } from 'app/components';
+import { View, Image, TouchableOpacity } from 'react-native';
+import { Button, Modal } from 'react-native-paper';
+import { Text, Wrapper, Header, DateTimePicker } from 'app/components';
 import Images from 'app/assets/images';
 import confirmationModel from 'app/models/ConfirmationModel';
 
 const FeedbackConfirmation = props => {
   const { navigation, route } = props;
   const [content, setContent] = useState('Hello');
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
 
   useEffect(() => {
     handleContent();
@@ -20,6 +24,7 @@ const FeedbackConfirmation = props => {
   };
 
   return (
+    <View style={{ flex: 1}}>
     <Wrapper>
       <Header
         headerRight={{
@@ -32,8 +37,23 @@ const FeedbackConfirmation = props => {
       <View style={{ flex: 2 }}>
         <Text type="h4">You did it!</Text>
         <Text type="body1">{content}</Text>
-      </View>
+        <TouchableOpacity
+          onPress={() => showModal()}
+        >
+          <Text>Press</Text>
+        </TouchableOpacity>
+      </View>      
     </Wrapper>
+    {/* <Modal
+        visible={isModalVisible}
+        onDismiss={hideModal}
+        contentContainerStyle={{ backgroundColor: 'white', padding: 30, width: 300, height: 400, }}
+      >
+        <DateTimePicker
+          mode='time'
+        />
+      </Modal> */}
+    </View>
   );
 };
 
