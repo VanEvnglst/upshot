cf8ab44c0279fd0385c6479e100571e3af5c7a13
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-09 20:12:51 +0800

    add data to data model

--- app/models/ConfirmationModel.js
+++ app/models/ConfirmationModel.js
@@ -6,8 +6,27 @@ const confirmationModel = [
   {
     id: 1,
     type: 'documenting',
-    content: ''
-  }
+    content: `${labels.feedbackDocumenting.content1} ${labels.feedbackDocumenting.content2}`,
+  },
+  {
+    id: 2,
+    type: 'preparing',
+    content: '',
+  },
+  {
+    id: 3,
+    type: 'discussing',
+    content: '',
+  },
+  {
+    id: 4,
+    type: 'reflecting',
+    content: '',
+  },
+  {
+    id: 5,
+    type: 'following up',
+  },
 ];
 
 export default confirmationModel;
