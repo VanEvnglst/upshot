3bfebdc32a1b0eba9f01a3b05f63d4584f903f17
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-09 13:44:38 +0800

    create confirmation data model

---
+++ app/models/ConfirmationModel.js
@@ -0,0 +1,13 @@
+import labels from 'app/locales/en';
+
+// const {}
+
+const confirmationModel = [
+  {
+    id: 1,
+    type: 'documenting',
+    content: ''
+  }
+];
+
+export default confirmationModel;
