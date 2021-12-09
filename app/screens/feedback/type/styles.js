68dc33ce8ec7cce59b7328e21beedc046ce03df8
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-09 14:59:16 +0800

    Create feedback type screen for new flow

---
+++ app/screens/feedback/type/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+import Colors from 'app/theme/colors';
+
+export default styles = StyleSheet.create({
+  contentContainer: {
+    flex: 5,
+    justifyContent: 'center',
+    padding: 10,
+  },
+  labelPadding: {
+    paddingBottom: 30
+  },
+  btnContainer :{
+    flex:1,
+    marginBottom: 20,
+    justifyContent: 'flex-end'
+  },
+  button: {
+    backgroundColor: Colors.primaryDark
+  },
+  disabledBtn: {
+    backgroundColor: Colors.black,
+    opacity: 0.04,
+  },
+  disabledText: {
+    color: Colors.opaqueBlack,
+  },
+  btnText: {
+    color: Colors.white
+  },
+});
