356c61579029949e6b63aedb4c9eb910dd3ae61e
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-09 20:13:40 +0800

    create confirmation screen

--- app/screens/feedback/confirmation/index.js
+++ app/screens/feedback/confirmation/index.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image } from 'react-native';
 import { Button } from 'react-native-paper';
-import { Text, Wrapper } from 'app/components'; 
+import { Text, Wrapper, Header } from 'app/components';
 import Images from 'app/assets/images';
+import confirmationModel from 'app/models/ConfirmationModel';
 
-const ConfirmationScreen = props => {
+const FeedbackConfirmation = props => {
   const { navigation, route } = props;
+  const [content, setContent] = useState('Hello');
 
-  return(
+  useEffect(() => {
+    handleContent();
+  }, []);
+
+  const handleContent = () => {
+    // get route.params.type
+    // const confirmationContent = confirmationModel.find(x => x.type === route.params.type)
+    //setContent(content);
+  };
+
+  return (
     <Wrapper>
-      <View style={{ flex: 1}}>
-        <Image
-          source={Images.confirmation}
-          resizeMode='contain'
-        />
+      <Header
+        headerRight={{
+          onPress: () => navigation.goBack(),
+        }}
+      />
+      <View style={{ flex: 1 }}>
+        <Image source={Images.confirmation} resizeMode="cover" />
       </View>
-      <View>
-        <Text type='h4'>You did it!</Text>
-        
+      <View style={{ flex: 2 }}>
+        <Text type="h4">You did it!</Text>
+        <Text type="body1">{content}</Text>
       </View>
     </Wrapper>
+  );
+};
 
-  )
-}
-
-export default ConfirmationScreen;
+export default FeedbackConfirmation;
