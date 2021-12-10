09488441bb9c284b7c3a26d6fd0605f4d3a2c0f8
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-10 16:34:13 +0800

    Update feedback confirmation screen with time picker

--- app/screens/feedback/confirmation/index.js
+++ app/screens/feedback/confirmation/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Image, TouchableOpacity } from 'react-native';
-import { Button, Modal } from 'react-native-paper';
+import { Button } from 'react-native-paper';
+import PropTypes from 'prop-types';
 import { Text, Wrapper, Header, DateTimePicker } from 'app/components';
 import Images from 'app/assets/images';
 import confirmationModel from 'app/models/ConfirmationModel';
@@ -9,7 +10,7 @@ const FeedbackConfirmation = props => {
   const { navigation, route } = props;
   const [content, setContent] = useState('Hello');
   const [isModalVisible, setModalVisible] = useState(false);
-
+  const [reminderTime, setReminderTime] = useState();
   const showModal = () => setModalVisible(true);
   const hideModal = () => setModalVisible(false);
 
@@ -23,36 +24,37 @@ const FeedbackConfirmation = props => {
     //setContent(content);
   };
 
+  const handleTimeSelection = time => {
+    console.log('tim', time);
+    hideModal();
+  };
+
   return (
-    <View style={{ flex: 1}}>
-    <Wrapper>
-      <Header
-        headerRight={{
-          onPress: () => navigation.goBack(),
-        }}
-      />
-      <View style={{ flex: 1 }}>
-        <Image source={Images.confirmation} resizeMode="cover" />
-      </View>
-      <View style={{ flex: 2 }}>
-        <Text type="h4">You did it!</Text>
-        <Text type="body1">{content}</Text>
-        <TouchableOpacity
-          onPress={() => showModal()}
-        >
-          <Text>Press</Text>
-        </TouchableOpacity>
-      </View>      
-    </Wrapper>
-    {/* <Modal
-        visible={isModalVisible}
-        onDismiss={hideModal}
-        contentContainerStyle={{ backgroundColor: 'white', padding: 30, width: 300, height: 400, }}
-      >
-        <DateTimePicker
-          mode='time'
+    <View style={{ flex: 1 }}>
+      <Wrapper>
+        <Header
+          headerRight={{
+            onPress: () => navigation.goBack(),
+          }}
         />
-      </Modal> */}
+        <View style={{ flex: 1 }}>
+          <Image source={Images.confirmation} resizeMode="cover" />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text type="h4">You did it!</Text>
+          <Text type="body1">{content}</Text>
+          <TouchableOpacity onPress={() => showModal()}>
+            <Text>Press</Text>
+          </TouchableOpacity>
+        </View>
+      </Wrapper>
+      <DateTimePicker
+        mode="time"
+        isVisible={isModalVisible}
+        value={reminderTime}
+        onCancel={() => hideModal()}
+        onConfirm={time => handleTimeSelection(time)}
+      />
     </View>
   );
 };
