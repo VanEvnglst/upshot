35f60969d265cb309046143e94554611437fbd6b
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-14 20:45:29 +0800

    Update navigation service

--- app/services/NavigationService.js
+++ app/services/NavigationService.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const navigationRef = React.createRef();
 
 export function navigate(name, params) {
-  if (navigationRef.isReady()) {
-    navigationRef.navigate(name, params);
-  }
+  navigationRef.current?.navigate(name, params);
+  // if (navigationRef.isReady()) {
+  //   navigationRef.navigate(name, params);
+  // }
 }
+
