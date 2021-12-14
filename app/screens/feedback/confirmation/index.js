54a4cd378e0526de316a0a25c8c61f508942b826
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-14 20:44:47 +0800

    Update feedback confirmation screen

--- app/screens/feedback/confirmation/index.js
+++ app/screens/feedback/confirmation/index.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { View, Image, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-paper';
+import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
 import PropTypes from 'prop-types';
+import { getDocumentingId } from 'app/store/selectors';
+import DocumentingActions from 'app/store/feedback/documentingRedux';
 import { Text, Wrapper, Header, DateTimePicker } from 'app/components';
 import Images from 'app/assets/images';
 import confirmationModel from 'app/models/ConfirmationModel';
 
 const FeedbackConfirmation = props => {
   const { navigation, route } = props;
-  const [content, setContent] = useState('Hello');
+  const dispatch = useDispatch();
+  const docuId = useSelector(getDocumentingId);
+  const [content, setContent] = useState();
   const [isModalVisible, setModalVisible] = useState(false);
   const [reminderTime, setReminderTime] = useState();
   const showModal = () => setModalVisible(true);
@@ -16,16 +22,48 @@ const FeedbackConfirmation = props => {
 
   useEffect(() => {
     handleContent();
+    // handleCallToAction();
   }, []);
 
   const handleContent = () => {
-    //get route.params.type
-    const confirmationContent = confirmationModel.find(x => x.type === route.params.type)
-    setContent(confirmationContent);
+    const confirmationContent = confirmationModel.find(
+      x => x.type === route.params.type,
+    );
+    setContent(confirmationContent.content);
   };
 
+  const handleCallToAction = () => {
+    const { type } = route.params;
+    switch (type) {
+      case 'documenting':
+        return <DocumentingCTA />;
+    }
+  };
+
+  const DocumentingCTA = () => {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <Button
+          type={'text'}
+          onPress={() => navigation.navigate('PreparingGuide')}>
+          <Text>Keep going</Text>
+        </Button>
+        <Button type={'text'} onPress={() => showModal()}>
+          <Text>remind me later</Text>
+        </Button>
+      </View>
+    );
+  };
+
+  const handleNavigation = () => {};
+
   const handleTimeSelection = time => {
-    console.log('tim', time);
+    const reminderDate = moment(time).format('MMM DD YYYY HH:mm');
+    const data = {
+      reminderDate,
+      docuId,
+    };
+    dispatch(DocumentingActions.updateDocumentingReminder(data));
     hideModal();
   };
 
@@ -34,18 +72,32 @@ const FeedbackConfirmation = props => {
       <Wrapper>
         <Header
           headerRight={{
-            onPress: () => navigation.goBack(),
+            onPress: () => navigation.navigate('ActiveFeedbackJourney'),
           }}
         />
-        <View style={{ flex: 1 }}>
-          <Image source={Images.confirmation} resizeMode="cover" />
+        <View style={{ flex: 2 }}>
+          <Image
+            source={Images.confirmation}
+            resizeMode="cover"
+            style={{ height: '90%' }}
+          />
         </View>
         <View style={{ flex: 2 }}>
           <Text type="h4">You did it!</Text>
-          <Text type="body1">{content}</Text>
-          {/* <TouchableOpacity onPress={() => showModal()}>
-            <Text>Press</Text>
-          </TouchableOpacity> */}
+          <Text
+            type="body1"
+            style={{ lineHeight: 24, marginTop: 35, width: '90%' }}>
+            {content}
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            marginBottom: 20,
+            alignItems: 'flex-end',
+            flexDirection: 'row',
+          }}>
+          {handleCallToAction()}
         </View>
       </Wrapper>
       <DateTimePicker
