8fba1aeb248b29e1b401d01aa86c0b1080c390f0
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-23 11:18:39 +0800

    create documenting review screen

--- app/screens/feedback/documenting/documenting-review/index.js
+++ app/screens/feedback/documenting/documenting-review/index.js
@@ -1,9 +1,35 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { Wrapper } from 'app/components';
+import { TouchableOpacity } from 'react-native';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Wrapper, Header, Text } from 'app/components';
 
 const DocumentingReview = props => {
-  return <Wrapper></Wrapper>;
+  const { navigation } = props;
+
+  const handleNavigation = () => {
+    console.log('press')
+  }
+  
+  return (
+  <Wrapper>
+    <Header
+      headerLeft={{
+        onPress: () => navigation.goBack()
+      }}
+    />
+    <Text type='overline'>Review</Text>
+    <Text type='h6'>Part 1 - Documenting</Text>
+    <View style={{ marginTop: 20, flexDirection: 'row', flexWrap: 'wrap'}}>
+      <Text type='body1' style={{ lineHeight: 48}}>I want to give
+      </Text>
+      <TouchableOpacity
+        onPress={() => handleNavigation()}
+      >
+        <Text type='body1'>corrective feedback</Text>
+      </TouchableOpacity>
+    </View>
+  </Wrapper>);
 };
 
 export default DocumentingReview;
