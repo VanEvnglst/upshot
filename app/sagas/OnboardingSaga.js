385ce47004773cab6ea30f75beec07d2d017d61f
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-23 11:11:53 +0800

    remove debugger

--- app/sagas/OnboardingSaga.js
+++ app/sagas/OnboardingSaga.js
@@ -11,7 +11,6 @@ export function* signUpUser({ data }) {
   //const connected = yield checkInternetConnection();
 
   const response = yield call(api.signUp, data);
-  debugger;
   if (response.ok) {
     if (response.data.result === RESULT_SUCCESS) {
       AsyncStorage.setItem('uniqueId', response.data.user.user_uuid);
