5fc38f3f4c6450cce52083a604f9082786a36b32
Ivan <engr.ivan.evangelista@gmail.com>
2021-12-29 11:00:53 +0800

    Create discussing screen

---
+++ app/screens/feedback/discussing/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import labels from 'app/locales/en';
+
+const FeedbackDiscussing = props => {};
+
+export default FeedbackDiscussing;
+
+FeedbackDiscussing.propTypes = {};
+
+FeedbackDiscussing.defaultProps = {};
