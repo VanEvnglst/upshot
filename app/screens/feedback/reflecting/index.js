98a0e0f40555e21f028cf8304800d5b9bba803ac
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-04 09:51:37 +0800

    create reflecting screen

---
+++ app/screens/feedback/reflecting/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import { Wrapper, Text } from 'app/components';
+
+const FeedbackReflecting = props => {};
+
+export default FeedbackReflecting;
+
+FeedbackReflecting.propTypes = {};
+
+FeedbackReflecting.defaultProps = {};
