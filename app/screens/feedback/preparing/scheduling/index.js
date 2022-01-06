7d16f2cb82f5258d55a691ec932d62540ce4ef4c
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-06 20:24:44 +0800

    Create preparing scheduling screen

---
+++ app/screens/feedback/preparing/scheduling/index.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+const PreparingSchedule = props => {
+
+  return (
+    <View>
+      
+    </View>
+  )
+};
+
+export default PreparingSchedule;
+
+PreparingSchedule.propTypes = {};
+
+PreparingSchedule.defaultProps = {};
