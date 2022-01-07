22c177cb8495f79bc79114a22de4aa31be67c5f3
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-07 20:41:47 +0800

    adjustments to preparing step 4B

--- app/screens/feedback/preparing/step4B/index.js
+++ app/screens/feedback/preparing/step4B/index.js
@@ -4,10 +4,7 @@ import { Button } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import PreparingActions from 'app/store/feedback/preparingRedux';
-import { 
-  getPreparingStep,
-  getPrepStep4BData
-} from 'app/store/selectors';
+import { getPreparingStep, getPrepStep4BData } from 'app/store/selectors';
 import { Text, ButtonSelection, TextInput } from 'app/components';
 import { preparingSuggestions } from 'app/models/PreparingModel';
 import labels from 'app/locales/en';
@@ -17,52 +14,59 @@ const PreparingStep4B = () => {
   const { createActionPlan } = labels.feedbackPreparing;
   const dispatch = useDispatch();
   const activeStep = useSelector(getPreparingStep);
-  const stepData = useSelector(getPrepStep4BData)
+  const stepData = useSelector(getPrepStep4BData);
   const [suggestionList, setSuggestionList] = useState([]);
   const [additionalSuggestion, setAdditionalSuggestion] = useState();
   const [isCompleted, setCompletion] = useState(false);
 
   useEffect(() => {
-    TODO: if(stepData.data) handleSelectedSuggestion(stepData.data);
-  },[stepData]);
+    TODO: if (stepData.data) handleSelectedSuggestion(stepData.data);
+  }, [stepData]);
 
   const handleBack = () => {
     dispatch(PreparingActions.setPrepActiveStep(activeStep - 1));
   };
 
   const handleNext = () => {
-    dispatch(PreparingActions.setPreparingData('step4B', suggestionList));
+    dispatch(
+      PreparingActions.setPreparingData('step4B', {
+        suggestionList,
+        additionalSuggestion,
+      }),
+    );
     dispatch(PreparingActions.setPrepActiveStep(activeStep + 1));
   };
 
+  const handleAdditionalSuggestionText = text => {
+    setAdditionalSuggestion(text);
+  };
+
   const checkSelectedSuggestion = item => {
-    return suggestionList.some(suggestion => suggestion === item);
-  }
+    return suggestionList.some(suggestion => suggestion === item.title);
+  };
 
   const handleSelectedSuggestion = item => {
     let newList = suggestionList;
-    if(checkSelectedSuggestion(item))
+    if (checkSelectedSuggestion(item))
       newList = newList.filter(newSuggestion => newSuggestion.id !== item.id);
-    else newList = [...newList, item];
+    else newList = [...newList, item.title];
     setSuggestionList(newList);
-  }
+  };
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <KeyboardAvoidingView>
         <View style={containerStyles.descriptionContainer}>
-          <Text 
-            type="h6" 
+          <Text
+            type="h6"
             style={containerStyles.stepTitleText}
-            testID={'txt-preparingStep4B-title'}
-          >
+            testID={'txt-preparingStep4B-title'}>
             {createActionPlan.step}: {createActionPlan.title}
           </Text>
-          <Text 
-            type="body1" 
+          <Text
+            type="body1"
             style={containerStyles.stepDescriptionText}
-            testID={'txt-preparingStep4B-content'}
-          >
+            testID={'txt-preparingStep4B-content'}>
             {createActionPlan.getSuggestions}
           </Text>
         </View>
@@ -76,11 +80,13 @@ const PreparingStep4B = () => {
             selected={checkSelectedSuggestion(item)}
           />
         ))}
-        <TextInput 
+        <TextInput
           label={labels.common.inputHint}
-          placeholder={labels.common.inputHint} 
+          placeholder={labels.common.inputHint}
           style={{ marginTop: 15 }}
           testID={'input-preparingStep4B-additional'}
+          onChangeText={text => handleAdditionalSuggestionText(text)}
+          value={additionalSuggestion}
         />
       </KeyboardAvoidingView>
       <View style={containerStyles.btnContainer}>
@@ -107,7 +113,7 @@ PreparingStep4B.propTypes = {
   setPrepActiveStep: PropTypes.func,
   setPreparingData: PropTypes.func,
   activeStep: PropTypes.number,
-  stepData: PropTypes.object
+  stepData: PropTypes.object,
 };
 
 PreparingStep4B.defaultProps = {
