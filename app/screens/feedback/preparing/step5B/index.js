93b94c7b31f95edc627497d1509e1533a22650c3
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-07 20:42:08 +0800

    adjustments to preparing step 5B

--- app/screens/feedback/preparing/step5B/index.js
+++ app/screens/feedback/preparing/step5B/index.js
@@ -14,79 +14,86 @@ const PreparingStep5B = () => {
   const dispatch = useDispatch();
   const activeStep = useSelector(getPreparingStep);
   const stepData = useSelector(getPrepStep5BData);
-  const [acknowledgeDetails, setAcknowledgeDetails] = useState();
+  const [acknowledgeDetails, setAcknowledgeDetails] = useState([]);
+  const [additionalAcknowledge, setAdditionalAcknowledge] = useState('');
   const [isCompleted, setCompletion] = useState(false);
 
   useEffect(() => {
     // if(stepData.data)
-  },[stepData]);
+  }, [stepData]);
 
   const handleBack = () => {
     dispatch(PreparingActions.setPrepActiveStep(activeStep - 1));
   };
 
   const handleNext = () => {
-    // dispatch(PreparingActions.setPreparingData())
+    dispatch(PreparingActions.setPreparingData('step5B', {
+      acknowledgeDetails,
+      additionalAcknowledge
+    }))
     dispatch(PreparingActions.setPrepActiveStep(activeStep + 1));
   };
 
+  const handleAcknowledgeText = text => setAdditionalAcknowledge(text)
+
   const checkSelectedValue = item => {
     return acknowledgeDetails.some(detail => detail == item);
-  }
+  };
 
   const handleSelectedValue = item => {
     let newList = acknowledgeDetails;
-    if(checkSelectedValue(item))
-      newList = newList.filter(newDetail => newDetail.id !== item.id);
+    if (checkSelectedValue(item))
+      newList = newList.filter(newDetail => newDetail !== item);
     else newList = [...newList, item];
     setAcknowledgeDetails(newList);
-  }
+  };
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <KeyboardAvoidingView>
         <View style={containerStyles.descriptionContainer}>
-          <Text 
-            type="h6" 
+          <Text
+            type="h6"
             style={containerStyles.stepTitleText}
-            testID={'txt-preparingStep5B-title'}  
-          >
+            testID={'txt-preparingStep5B-title'}>
             {checkOut.step}: {checkOut.title}
           </Text>
-          <Text 
-            style="body1" 
+          <Text
+            style="body1"
             style={containerStyles.stepDescriptionText}
-            testID={'txt-preparingStep5B-description'}
-          >
+            testID={'txt-preparingStep5B-description'}>
             {checkOut.checkoutAcknowledge}
           </Text>
         </View>
+        <View style={{flex: 1, marginBottom: 60, }}>
         <ButtonSelection
           type={'Check'}
           title={checkOut.checkoutInput}
-          selected={checkSelectedValue()}
-          onPress={() => handleSelectedValue()}
+          selected={checkSelectedValue(checkOut.checkoutInput)}
+          onPress={() => handleSelectedValue(checkOut.checkoutInput)}
           testID={'btn-preparingStep5B-detail'}
+          style={{ height: 120 }}
         />
         <TextInput
           label={labels.common.inputHint}
           placeholder={labels.common.inputHint}
           testID={'input-preparingStep5B-somethingElse'}
+          onChangeText={text => handleAcknowledgeText(text)}
+          value={additionalAcknowledge}
         />
+        </View>
       </KeyboardAvoidingView>
       <View style={containerStyles.btnContainer}>
-      <Button
-          mode='text'
+        <Button
+          mode="text"
           onPress={() => handleBack()}
-          testID={'btn-preparingStep5B-back'}
-        >
+          testID={'btn-preparingStep5B-back'}>
           {labels.common.back}
         </Button>
         <Button
           onPress={() => handleNext()}
           mode={'contained'}
-          testID={'btn-preparingStep5B-next'}
-        >
+          testID={'btn-preparingStep5B-next'}>
           {labels.common.next}
         </Button>
       </View>
@@ -100,7 +107,7 @@ PreparingStep5B.propTypes = {
   setPrepActiveStep: PropTypes.func,
   setPreparingData: PropTypes.func,
   activeStep: PropTypes.number,
-  stepData: PropTypes.object
+  stepData: PropTypes.object,
 };
 
 PreparingStep5B.defaultProps = {
