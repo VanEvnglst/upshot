7d9d1fde69662df9b04f12e58e105b51f906bfc8
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-12 19:07:17 +0800

    add assets

--- app/assets/images.js
+++ app/assets/images.js
@@ -21,11 +21,11 @@ export default {
   schedule: require('./images/schedule.png'),
   manWindow: require('./images/window.png'),
   confirmation: require('./images/confirmation.png'),
-  preparingGuide: require('./images/preparing-guide.png'),
   preparingConfirmHint: require('./images/preparing-confirm-hint.png'),
   cueCards: require('./images/cue-cards.png'),
   voiceRecording: require('./images/voice-recording.png'),
   meetingNotes: require('./images/meeting-notes.png'),
   cardSwipeGuide: require('./images/card-swipe.png'),
+  reflectingGuide: require('./images/reflecting-guide.png'),
  
 };
