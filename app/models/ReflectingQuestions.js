1194d870b5bfa2c2b19e671e3c1c1eb0e74898a0
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-13 19:53:53 +0800

    create question data model

---
+++ app/models/ReflectingQuestions.js
@@ -0,0 +1,40 @@
+import labels from 'app/locales/en';
+
+const { feedbackReflecting } = labels;
+
+const reflectingQuestions = [
+  {
+    id: 1,
+    question: feedbackReflecting.provideInfo
+  },
+  {
+    id: 2,
+    question: feedbackReflecting.calmFeedback
+  },
+  {
+    id: 3,
+    question: feedbackReflecting.listenToEmployee
+  },
+  {
+    id: 4,
+    question: feedbackReflecting.gaveFeedbackSoon
+  },
+  {
+    id: 5,
+    question: feedbackReflecting.establishRapport
+  },
+  {
+    id: 6,
+    question: feedbackReflecting.clearlyStatePurpose
+  },
+  {
+    id: 7,
+    question: feedbackReflecting.involveEmployee
+  },
+  {
+    id: 8,
+    question: feedbackReflecting.documentAndSend
+  },
+];
+
+export default reflectingQuestions;
