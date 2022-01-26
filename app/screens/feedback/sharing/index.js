78ad73e53ae4d20b5db957bb540b6c46756b2a8d
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-26 12:31:11 +0800

    create feedback sharing screen

---
+++ app/screens/feedback/sharing/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+
+const FeedbackSharing = () => {
+return(
+  <View>
+
+  </View>
+);
+}
+
+export default FeedbackSharing;
+
+FeedbackSharing.propTypes = {};
+
+
+FeedbackSharing.defaultProps = {};
