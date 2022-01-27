22d40c5c2c082271bd602ecd025cea53255888ab
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-27 17:49:00 +0800

    update data models for journey steps

---
+++ app/models/ScheduledPositiveSteps.js
@@ -0,0 +1,23 @@
+import labels from '../locales/en';
+
+const { feedbackSignPost } = labels;
+
+const scheduledPositiveSteps = [
+  {
+    id: 1,
+    title: feedbackSignPost.documenting,
+    description: feedbackSignPost.documentingDesc,
+  },
+  {
+    id: 2,
+    title: feedbackSignPost.sharing,
+    description: feedbackSignPost.sharingDesc,
+  },
+  {
+    id: 3,
+    title: feedbackSignPost.reflecting,
+    description: feedbackSignPost.reflectingDesc,
+  },
+];
+
+export default scheduledPositiveSteps;
