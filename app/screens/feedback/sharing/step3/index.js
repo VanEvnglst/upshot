1a92db6c4c718b5eaf40ebf321cfdd4c25aeeb74
Ivan <engr.ivan.evangelista@gmail.com>
2022-01-31 19:22:44 +0800

    update sharing step 3 screen

--- app/screens/feedback/sharing/step3/index.js
+++ app/screens/feedback/sharing/step3/index.js
@@ -1,9 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { Button } from 'react-native-paper';
+import { useDispatch, useSelector} from 'react-redux';
 import { Text } from 'app/components';
+import labels from 'app/locales/en';
+import containerStyles from '../styles';
+import styles from './styles';
 
 const SharingStep3 = () => {
-  return <View></View>;
+  const { shareFeedback } = labels.feedbackSharing;
+  const dispatch = useDispatch();
+
+  return (
+  <ScrollView
+    showsVerticalScrollIndicator={false}
+    bounces={false}
+  >
+    <View style={containerStyles.descriptionContainer}>
+      <Text
+        type='h6'
+        style={containerStyles.stepTitleText}
+        testID={'txt-sharingStep3-title'}
+      >
+        {shareFeedback.step}: {shareFeedback.title}
+      </Text>
+      <Text
+        type='body1'
+        style={containerStyles.stepDescriptionText}
+        testID={'txt-sharingStep3-description'}
+      >
+        {shareFeedback.content}
+      </Text>
+    </View>
+    <View style={{ flex: 3}}>
+      <View style={{ borderWidth: 1, borderRadius: 8, padding: 20,}}
+      >
+        <View style={{  flexDirection: 'row'}}>
+          <View style={{ height: 40, width: 40, borderRadius: 20, backgroundColor: 'red'}}
+          />
+          <View style={{ marginLeft: 20}}>
+          <Text>Name</Text>
+          <Text>To: staff</Text>
+          </View>
+        </View>
+        <View style={{ marginTop: 30}}>
+          <Text>Text description</Text>
+        </View>
+      </View>
+    </View>
+    <View style={containerStyles.btnContainer}>
+      <Button
+        onPress={() => handleBack()}
+        mode='text'
+        testID={'btn-sharingStep3-back'}
+      >
+        {labels.common.back}
+      </Button>
+      <Button
+        onPress={() => handleNext()}
+        mode='contained'
+        testID={'btn-sharingStep3-next'}
+      >
+        {labels.common.next}
+      </Button>
+    </View>
+  </ScrollView>
+  );
 };
 
 export default SharingStep3;
