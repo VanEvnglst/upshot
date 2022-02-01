7fc9f8ed7d217a2d090f0937cdd5708a36f705cc
Ivan <engr.ivan.evangelista@gmail.com>
2022-02-01 14:13:38 +0800

    update sharing step 3 screen and styles

--- app/screens/feedback/sharing/step3/index.js
+++ app/screens/feedback/sharing/step3/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Image, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-paper';
-import { useDispatch, useSelector} from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useDispatch, useSelector } from 'react-redux';
 import { Text } from 'app/components';
+import Images from 'app/assets/images';
 import labels from 'app/locales/en';
 import containerStyles from '../styles';
 import styles from './styles';
@@ -11,60 +13,110 @@ const SharingStep3 = () => {
   const { shareFeedback } = labels.feedbackSharing;
   const dispatch = useDispatch();
 
-  return (
-  <ScrollView
-    showsVerticalScrollIndicator={false}
-    bounces={false}
-  >
-    <View style={containerStyles.descriptionContainer}>
-      <Text
-        type='h6'
-        style={containerStyles.stepTitleText}
-        testID={'txt-sharingStep3-title'}
-      >
-        {shareFeedback.step}: {shareFeedback.title}
-      </Text>
-      <Text
-        type='body1'
-        style={containerStyles.stepDescriptionText}
-        testID={'txt-sharingStep3-description'}
-      >
-        {shareFeedback.content}
-      </Text>
-    </View>
-    <View style={{ flex: 3}}>
-      <View style={{ borderWidth: 1, borderRadius: 8, padding: 20,}}
-      >
-        <View style={{  flexDirection: 'row'}}>
-          <View style={{ height: 40, width: 40, borderRadius: 20, backgroundColor: 'red'}}
-          />
-          <View style={{ marginLeft: 20}}>
-          <Text>Name</Text>
-          <Text>To: staff</Text>
-          </View>
+  const EditButton = () => {
+    return (
+      <TouchableOpacity
+        accessibilityRole="button"
+        style={styles.editButton}
+        onPress={() => handleEditNavigation()}>
+        <View style={styles.editContainer}>
+          {/* <Icon
+                name='mode_edit'
+                size={18}
+              /> */}
+          <Text type="button" style={styles.editText}>
+            Edit
+          </Text>
         </View>
-        <View style={{ marginTop: 30}}>
-          <Text>Text description</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const handleEditNavigation = () => {};
+
+  const handleBack = () => {};
+
+  const handleNext = () => {};
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} bounces={false}>
+      <View style={containerStyles.descriptionContainer}>
+        <Text
+          type="h6"
+          style={containerStyles.stepTitleText}
+          testID={'txt-sharingStep3-title'}>
+          {shareFeedback.step}: {shareFeedback.title}
+        </Text>
+        <Text
+          type="body1"
+          style={containerStyles.stepDescriptionText}
+          testID={'txt-sharingStep3-description'}>
+          {shareFeedback.content}
+        </Text>
+      </View>
+      <View style={styles.content}>
+        <View style={styles.reviewCard}>
+          <View style={styles.nameContainer}>
+            <Image source={Images.avatar} style={styles.avatar} />
+            <View style={styles.nameContent}>
+              <Text type="caption" style={styles.managerNameText}>
+                Ivan Evangelista
+              </Text>
+              <Text type="caption" style={styles.staffNameText}>
+                To: staff
+              </Text>
+            </View>
+          </View>
+          <View style={{ marginTop: 30 }}>
+            <Text type="body2"
+              style={styles.bodyText}
+            >Text description</Text>
+            <EditButton />
+          </View>
+          <View style={styles.earContainer}>
+          <View style={styles.earContent}>
+            <Text type="overline" style={styles.overlineEAR}>
+                Event
+              </Text>
+              <Text type="body2" style={styles.bodyText}>
+                Lorem ipsum
+              </Text>
+            </View>
+            <View style={styles.earContent}>
+            <Text type="overline" style={styles.overlineEAR}>
+                Action
+              </Text>
+              <Text type="body2" style={styles.bodyText}>
+                Lorem ipsum
+              </Text>
+            </View>
+            <View style={styles.earContent}>
+            <Text type="overline" style={styles.overlineEAR}>
+                Result
+              </Text>
+              <Text type="body2" style={styles.bodyText}>
+                Lorem ipsum
+              </Text>
+            </View>
+          </View>
+          <EditButton />
         </View>
       </View>
-    </View>
-    <View style={containerStyles.btnContainer}>
-      <Button
-        onPress={() => handleBack()}
-        mode='text'
-        testID={'btn-sharingStep3-back'}
-      >
-        {labels.common.back}
-      </Button>
-      <Button
-        onPress={() => handleNext()}
-        mode='contained'
-        testID={'btn-sharingStep3-next'}
-      >
-        {labels.common.next}
-      </Button>
-    </View>
-  </ScrollView>
+      <View style={containerStyles.btnContainer}>
+        <Button
+          onPress={() => handleBack()}
+          mode="text"
+          testID={'btn-sharingStep3-back'}>
+          {labels.common.back}
+        </Button>
+        <Button
+          onPress={() => handleNext()}
+          mode="contained"
+          testID={'btn-sharingStep3-next'}>
+          Send
+        </Button>
+      </View>
+    </ScrollView>
   );
 };
 
