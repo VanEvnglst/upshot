bf71ccbbcf08db7451f902f5d036ceda7919d474
Ivan <engr.ivan.evangelista@gmail.com>
2022-02-07 15:48:39 +0800

    integrate update and fetching sharing api

--- app/sagas/feedback/SharingSaga.js
+++ app/sagas/feedback/SharingSaga.js
@@ -29,14 +29,15 @@ export function* postFeedbackSharing({ journeyId }) {
 export function* updateFeedbackSharing({ data }) {
   //const connected = yield checkInternetConnection();
   // if(!connected) { return };
+  const { message, details } = data;
   debugger;
   const params = new URLSearchParams();
   const sharingId = yield select(sharing);
   params.append('sharing_id', sharingId);
-  // params.append('event_str')
-  // params.append('action_str');
-  // params.append('result_str');
-  // params.append('message_str');
+  params.append('event_str', details.event);
+  params.append('action_str', details.action);
+  params.append('result_str', details.result);
+  params.append('message_str', message);
 
   const response = yield call(api.updateFeedbackSharing, params);
   if (response.ok) {
@@ -52,12 +53,11 @@ export function* updateFeedbackSharing({ data }) {
   }
 }
 
-export function* updateFeedbackSharingReminder({ data }) {
+export function* updateSharingReminder({ data }) {
   const params = new URLSearchParams();
-  debugger;
   const sharingId = yield select(sharing);
   params.append('sharing_id', sharingId);
-  // params.append('reminder_datetime', )
+  params.append('reminder_datetime', data.reminderDate)
   
   const response = yield call(api.updateFeedbackSharing, params);
   if(response.ok) {
@@ -74,14 +74,17 @@ export function* fetchCurrentSharing({ sharingId }) {
   // const connected = yield checkInternetConnection();
   // if (!connected) { return };
   const params = new URLSearchParams();
-  // const sharingId = yield select(sharing);
   params.append('sharing_id', sharingId);
 
   const response = yield call(api.getCurrentSharing, params);
-  debugger
   if (response.ok) {
     if (response.data.status === STATUS_OK) {
-      //TODO: set items to state if success
+      const sharingDetails = response.data.details;
+      const { event, action, result, message } = sharingDetails;
+      yield put(SharingActions.setSharingData('step1', {
+        event, action, result
+      }));
+      yield put(SharingActions.setSharingData('step2', message));
       yield put(SharingActions.fetchCurrentSharingSuccess())
     }
   } else {
@@ -115,7 +118,7 @@ export function* closeFeedbackSharing({ sharingId }) {
 function* watchSharingSaga() {
   yield takeLatest(SharingTypes.POST_FEEDBACK_SHARING, postFeedbackSharing);
   yield takeLatest(SharingTypes.UPDATE_FEEDBACK_SHARING, updateFeedbackSharing);
-  yield takeLatest(SharingTypes.UPDATE_FEEDBACK_SHARING_REMINDER, updateFeedbackSharingReminder);
+  yield takeLatest(SharingTypes.UPDATE_SHARING_REMINDER, updateSharingReminder);
   yield takeLatest(SharingTypes.FETCH_CURRENT_SHARING, fetchCurrentSharing);
   yield takeLatest(SharingTypes.CLOSE_FEEDBACK_SHARING, closeFeedbackSharing);
 }
