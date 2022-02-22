67b1b52556f27ba3a4c5e1ebdaf4ea049aa16d10
Ivan <engr.ivan.evangelista@gmail.com>
2022-02-22 14:03:43 +0800

    create messages saga

---
+++ app/sagas/MessagesSaga.js
@@ -0,0 +1,31 @@
+import { checkInternetConnection } from 'react-native-offline';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
+import MessagesActions, { MessagesTypes } from 'app/store/MessagesRedux';
+import api from 'app/services/apiService';
+
+const STATUS_OK = 'ok';
+
+export function* fetchMessages({}) {
+  // const connected = yield checkInternetConnection();
+  // if (!connected ) { return; }
+  console.warn('call api');
+  const response = yield call(api.getFrontlinerMessages);
+  debugger;
+  if(response.ok) {
+    if (response.data.status === STATUS_OK) {
+      const messagesList = response.data.inbox;
+      yield put(MessagesActions.fetchMessagesSuccess(messagesList))
+    } else {
+      yield put(MessagesActions.fetchMessagesFailure())
+    }
+  } else {
+    yield put(MessagesActions.fetchMessagesFailure(response.data))
+  }
+}
+
+
+function* watchMessagesSaga() {
+  yield takeLatest(MessagesTypes.FETCH_MESSAGES, fetchMessages);
+}
+
+export default watchMessagesSaga;
