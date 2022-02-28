bbb37ecc822f11155c7d69cc311b7f809fffeee0
Ivan <engr.ivan.evangelista@gmail.com>
2022-02-28 20:11:28 +0800

    create survey confirmation screen

---
+++ app/screens/frontliner/survey/confirmation/index.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'react-native-paper';
+import PropTypes from 'prop-types';
+import { Text, Slider } from 'app/components';
+import Images from 'app/assets/images';
+import labels from 'app/locales/en';
+import containerStyles from '../styles';
+
+
+const SurveyConfirmation = props => {
+  return (
+    <View></View>
+  )
+}
+
+
+export default SurveyConfirmation;
+
+SurveyConfirmation.propTypes = {};
+
+SurveyConfirmation.defaultProps = {};
