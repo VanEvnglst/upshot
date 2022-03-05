76efc4f0750181aecfecb7f7a70ffcf33ffd099a
Ivan <engr.ivan.evangelista@gmail.com>
2022-03-05 16:35:41 +0800

    Adjustments to input field component

---
+++ app/components/input-field/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import Colors from 'app/theme/colors';
+
+export default styles = StyleSheet.create({
+  descriptionText: { 
+    paddingLeft: 10, 
+    paddingTop: 5,
+    color: Colors.lightBlack
+   },
+   focusedDescription: {
+     color: Colors.primary,
+   }
+});
