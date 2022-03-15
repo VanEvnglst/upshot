ba564ef4842cf7dca4da44a668cdd43e1d60ad27
Ivan <engr.ivan.evangelista@gmail.com>
2022-03-15 21:02:23 +0800

    adjustments to input field component

--- app/components/input-field/index.js
+++ app/components/input-field/index.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 import { TextInput as Input } from 'react-native-paper';
+import PropTypes from 'prop-types';
 import { Text } from 'app/components';
+import Colors from 'app/theme/colors';
 import styles from './styles';
 
+
 const TextInput = props => {
-  const { label, placeholder } = props;
+  const { label, placeholder, description, error } = props;
   const [isFocused, setFocus] = useState(false);
   return (
-    <View>
+    <View style={styles.container}>
       <Input
         type="flat"
         textAlignVertical={'top'}
@@ -17,15 +20,44 @@ const TextInput = props => {
         placeholder={isFocused ? null : placeholder}
         onBlur={() => setFocus(false)}
         onFocus={() => setFocus(true)}
+        activeUnderlineColor={Colors.primaryDark}
       />
-      <Text 
-        type="caption" 
-        style={[styles.descriptionText,
-        isFocused && styles.focusedDescription]}>
-        Write your own description
-      </Text>
+      {description && !error && (
+        <Text
+          type="caption"
+          style={[
+            styles.descriptionText,
+            isFocused && styles.focusedDescription,
+          ]}>
+          {description}
+        </Text>
+      )}
+      {error && error.length !== 0 && (
+        <Text
+          type='caption'
+          style={[
+            styles.descriptionText,
+            styles.errorText
+          ]}
+        >{error}</Text>
+      )}
     </View>
   );
 };
 
 export default TextInput;
+
+TextInput.propTypes = {
+  label: PropTypes.any,
+  placeholder: PropTypes.any,
+  description: PropTypes.any,
+  error: PropTypes.any,
+};
+
+
+TextInput.defaultProps = {
+  label: null,
+  placeholder: null,
+  description: null,
+  error: null,
+};
