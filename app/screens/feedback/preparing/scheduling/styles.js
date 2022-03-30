8d46342cbacecf2a31cbfcdc1ab6ac83d6f1b4fa
Ivan <engr.ivan.evangelista@gmail.com>
2022-03-30 17:41:40 +0800

    adjustments to data handling inside scheduling

--- app/screens/feedback/preparing/scheduling/styles.js
+++ app/screens/feedback/preparing/scheduling/styles.js
@@ -3,7 +3,14 @@ import Colors from 'app/theme/colors';
 
 export default styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+  },
+  contentContainer: { 
+     flex: 7,
+  },
+  selectionContainer: {
+    flex: 1,
+    marginTop: 30,
   },
   hintCard: {
     backgroundColor: 'rgba(0,0,0,0.04)',
@@ -18,6 +25,30 @@ export default styles = StyleSheet.create({
     lineHeight: 24,
     fontWeight: '700',
     textAlign: 'center',
-    width: '85%'
+    width: '85%',
   },
-})
+  spacer: {
+    marginBottom: 10,
+  },
+  btnContainer: { 
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 20,
+  },
+  inviteBtn: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    padding: 20,
+    width: 350,
+    height: 550,
+  },
+  alertConfirmationContainer: {
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+    alignItems: 'flex-end',
+    marginTop: 20,
+  },
+});
