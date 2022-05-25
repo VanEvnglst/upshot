88827f620d85a13e35171b00549ec7ba0c19bc8e
Ivan <engr.ivan.evangelista@gmail.com>
2022-05-25 20:00:11 +0800

    add selector

--- app/store/selectors.js
+++ app/store/selectors.js
@@ -59,6 +59,7 @@ export const getPrepStep4BData = state => state.preparing.get('step4B');
 export const getPrepStep5Data = state => state.preparing.get('step5');
 export const getPrepStep5BData = state => state.preparing.get('step5B');
 export const getPrepStep5CData = state => state.preparing.get('step5C');
+export const getPreparingSched = state => state.preparing.get('discussionSchedule');
 
 /* ------------- Feedback Discussing Selectors ------------- */
 export const getDiscussing = state => state.discussing;
