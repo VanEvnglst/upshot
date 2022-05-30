6f5a5d85c00804ea9d83e4d97a93e089c30beeb8
Ivan <engr.ivan.evangelista@gmail.com>
2022-05-30 18:48:52 +0800

    handle navigation for feeling question

--- app/screens/frontliner/survey/feeling-question/index.js
+++ app/screens/frontliner/survey/feeling-question/index.js
@@ -4,13 +4,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import { Text, Slider } from 'app/components';
+import SurveyActions from 'app/store/frontliner/SurveyRedux';
+import { getSurveyStep } from 'app/store/selectors';
 import Images from 'app/assets/images';
 import labels from 'app/locales/en';
 import containerStyles from '../styles';
 
 const FeelingQuestion = props => {
   const { survey } = labels.frontliner;
+  const dispatch = useDispatch();
+  const activeStep = useSelector(getSurveyStep);
   const [feelingValue, setFeelingValue] = useState(0);
+
+  
+  const handleBack = () => {
+    dispatch(SurveyActions.setSurveyActiveStep(activeStep - 1));
+  }
+
+  const handleNext = () => {
+    dispatch(SurveyActions.setSurveyActiveStep(activeStep + 1));
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <View style={{ marginTop: 30 }}>
@@ -39,9 +53,11 @@ const FeelingQuestion = props => {
       <View style={containerStyles.btnContainer}>
         <Button
           mode='text'
+          onPress={() => handleBack()}
         >{labels.common.back}</Button>
         <Button
           mode='contained'
+          onPress={() => handleNext()}
         >{labels.common.next}</Button>
       </View>
     </View>
@@ -50,6 +66,14 @@ const FeelingQuestion = props => {
 
 export default FeelingQuestion;
 
-FeelingQuestion.propTypes = {};
+FeelingQuestion.propTypes = {
+  getSurveyStep: PropTypes.number,
+  setSurveyActiveStep: PropTypes.func,
+  // setSurveyData: PropTypes.func
+};
 
-FeelingQuestion.defaultProps = {};
+FeelingQuestion.defaultProps = {
+  getSurveyStep: 1,
+  setSurveyActiveStep: () => {},
+  //setSurveyData: () => {},
+};
