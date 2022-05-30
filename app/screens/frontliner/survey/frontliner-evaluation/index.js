7683ff57bc2917699b37350deb631d189a867765
Ivan <engr.ivan.evangelista@gmail.com>
2022-05-30 18:49:03 +0800

    handle navigation for frontliner eval question

--- app/screens/frontliner/survey/frontliner-evaluation/index.js
+++ app/screens/frontliner/survey/frontliner-evaluation/index.js
@@ -6,12 +6,25 @@ import PropTypes from 'prop-types';
 import { Text, Slider } from 'app/components';
 import Images from 'app/assets/images';
 import { frontlinerEval } from 'app/models/FrontlinerEvalModel';
+import SurveyActions from 'app/store/frontliner/SurveyRedux';
+import { getSurveyStep } from 'app/store/frontliner/SurveyRedux';
 import labels from 'app/locales/en';
 import containerStyles from '../styles';
 
 
 const FrontlinerEvaluation = props => {
   const { survey } = labels.frontliner;
+  const dispatch = useDispatch();
+  const activeStep = useSelector(getSurveyStep);
+
+
+  const handleBack = () => {
+    dispatch(SurveyActions.setSurveyActiveStep(activeStep - 1));
+  }
+
+  const handleNext = () => {
+    dispatch(SurveyActions.setSurveyActiveStep(activeStep + 1));
+  }
 
   return (
     <View style={{ flex: 1 }}>
@@ -22,6 +35,16 @@ const FrontlinerEvaluation = props => {
           testID={'txt-frontlinerSurvey-frontlinerEval'}
         >{survey.selfSurvey}</Text>
       </View>
+      <View style={containerStyles.btnContainer}>
+        <Button
+          mode='text'
+          onPress={() => handleBack()}
+        >{labels.common.back}</Button>
+        <Button
+          mode='contained'
+          onPress={() => handleNext()}
+        >{labels.common.next}</Button>
+      </View>
     </View>
   )
 }
@@ -29,6 +52,14 @@ const FrontlinerEvaluation = props => {
 export default FrontlinerEvaluation;
 
 
-FrontlinerEvaluation.propTypes = {};
+FrontlinerEvaluation.propTypes = {
+  getSurveyStep: PropTypes.number,
+  setSurveyActiveStep: PropTypes.func,
+  // setSurveyData: PropTypes.func,
+};
 
-FrontlinerEvaluation.defaultProps = {};
+FrontlinerEvaluation.defaultProps = {
+  getSurveyStep: 1,
+  setSurveyActiveStep: () => {},
+  //setSurveyData: () => {},
+};
