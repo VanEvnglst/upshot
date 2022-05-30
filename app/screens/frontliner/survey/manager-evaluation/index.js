7426666caf109563313d2d3b2e41508e0ae064c0
Ivan <engr.ivan.evangelista@gmail.com>
2022-05-30 18:49:11 +0800

    handle navigation for manager eval question

--- app/screens/frontliner/survey/manager-evaluation/index.js
+++ app/screens/frontliner/survey/manager-evaluation/index.js
@@ -5,12 +5,25 @@ import { Button } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import { Text, Slider } from 'app/components';
 import {managerEval} from 'app/models/ManagerEvalModel';
+import SurveyActions from 'app/store/frontliner/SurveyRedux';
+import { getSurveyStep } from 'app/store/selectors';
 import Images from 'app/assets/images';
 import labels from 'app/locales/en';
 import containerStyles from '../styles';
 
 const ManagerEvaluation = props => {
   const { survey } = labels.frontliner;
+  const dispatch = useDispatch();
+  const activeStep = useSelector(getSurveyStep);
+
+  const handleBack = () => {
+    dispatch(SurveyActions.setSurveyActiveStep(activeStep - 1));
+  }
+
+  const handleNext = () => {
+    dispatch(SurveyActions.setSurveyActiveStep(activeStep + 1));
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <View style={{ marginTop: 30 }}>
@@ -20,6 +33,16 @@ const ManagerEvaluation = props => {
           testID={'txt-frontlinerSurvey-managerEval'}
         >{survey.howDidManagerDo} {survey.howDidManagerDoCont}</Text>
       </View>
+      <View style={containerStyles.btnContainer}>
+        <Button
+          mode='text'
+          onPress={() => handleBack()}
+        >{labels.common.back}</Button>
+        <Button
+          mode='contained'
+          onPress={() => handleNext()}
+          >{labels.common.next}</Button>
+      </View>
     </View>
   )
 }
@@ -28,6 +51,14 @@ const ManagerEvaluation = props => {
 export default ManagerEvaluation;
 
 
-ManagerEvaluation.propTypes = {};
+ManagerEvaluation.propTypes = {
+  getSurveyStep: PropTypes.number,
+  setSurveyActiveStep: PropTypes.func,
+  //setSurveyData: PropTypes.func,
+};
 
-ManagerEvaluation.defaultProps = {};
+ManagerEvaluation.defaultProps = {
+  getSurveyStep: 1,
+  setSurveyActiveStep: () => {},
+  //setSurveyData: () => {},
+};
