17c400c352dc660520e619267580059dcd4c8dd5
Ivan <engr.ivan.evangelista@gmail.com>
2022-06-01 19:26:31 +0800

    create survey confirmation screen

--- app/screens/frontliner/survey/confirmation/index.js
+++ app/screens/frontliner/survey/confirmation/index.js
@@ -1,17 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-native-paper';
 import PropTypes from 'prop-types';
-import { Text, Slider } from 'app/components';
+import { Text, Wrapper, Header } from 'app/components';
 import Images from 'app/assets/images';
 import labels from 'app/locales/en';
-import containerStyles from '../styles';
+import styles from './styles';
+
 
 
 const SurveyConfirmation = props => {
+  const { navigation } = props;
+
+  const handleClose = () => {
+
+  }
+
+  const handleContent = () => {
+    //TODO: handle if confirmation or no survey
+    return (
+      <>
+        <View>
+          <Image
+            source={Images.noRating}
+          />
+          <View style={styles.contentContainer}>
+
+          </View>
+        </View>
+      </>
+    )
+  }
   return (
-    <View></View>
+    <View style={styles.container}>
+      <Wrapper>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+        >
+          <Header
+            headerRight={{
+              onPress: () => {}
+            }}
+          />
+          {handleContent()}
+          <View style={styles.btnContainer}>
+            <View
+              style={{
+                marginBottom: 20,
+                flex: 1
+              }}>
+                <Button
+                  mode='contained'
+                  onPress={() => console.log('close')}
+                  >Close</Button>
+              </View>
+          </View>
+          {/* <View>
+            <Image
+              source={Images.confirmation}
+              resizeMode='cover'
+            />
+          </View>
+          <View style={styles.contentContainer}>
+          </View> */}
+        </ScrollView>
+      </Wrapper>
+    </View>
   )
 }
 
---
+++ app/screens/frontliner/survey/confirmation/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import Colors from 'app/theme/colors';
+
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    marginTop: 30,
+  },
+});
