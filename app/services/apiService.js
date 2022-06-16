e967f5ca1539e2bff0eb52adaf72fd2eb219c64d
Ivan <engr.ivan.evangelista@gmail.com>
2022-06-16 18:59:54 +0800

    fix url for new survey

--- app/services/apiService.js
+++ app/services/apiService.js
@@ -332,7 +332,7 @@ export default {
     const upshotAPI = await setAPI(url);
     const uniqueId = await AsyncStorage.getItem('uniqueId');
 
-    return upshotAPI.post(`/${uniqueId}/`, payload);
+    return upshotAPI.post(`/${uniqueId}/feedback/frontliner/feedback/survey`, payload);
   },
 
   updateDRSurvey: async payload => {
@@ -340,7 +340,7 @@ export default {
     const upshoAPI = await setAPI(url);
     const uniqueId = await AsyncStorage.getItem('uniqueId');
 
-    return upshotAPI.post(`/${uniqueId}/`, payload);
+    return upshotAPI.post(`/${uniqueId}/feedback/frontliner/feedback/survey/edit`, payload);
   },
 
   getCurrentDRSurvey: async payload => {
