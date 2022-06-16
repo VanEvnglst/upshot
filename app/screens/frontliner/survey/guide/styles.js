650e33ca6e8b7faf65beac427e0716a2445f5082
Ivan <engr.ivan.evangelista@gmail.com>
2022-06-16 19:00:25 +0800

    update survey guide screen

--- app/screens/frontliner/survey/guide/styles.js
+++ app/screens/frontliner/survey/guide/styles.js
@@ -2,5 +2,27 @@ import { StyleSheet } from 'react-native';
 import Colors from 'app/theme/colors';
 
 export default styles = StyleSheet.create({
-
+contentContainer: {
+  flex: 2
+},
+descriptionContainer: { 
+  flex: 1,
+  marginTop: 30, 
+  marginBottom: 20 
+},
+descriptionText: {
+  lineHeight: 30 
+},
+imageContainer: {
+  flex: 1, 
+  marginBottom: 20
+},
+btnContainer: {
+  marginBottom: 30
+},
+button: {
+  height: 50, 
+  justifyContent: 'center', 
+  alignItems: 'center'
+}
 });
