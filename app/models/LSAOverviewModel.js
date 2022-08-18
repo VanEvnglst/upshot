4758c6b1ecd1967719f2bc80620785da2f41a01c
Ivan <engr.ivan.evangelista@gmail.com>
2022-08-18 22:27:28 +0800

    create overview questions model

---
+++ app/models/LSAOverviewModel.js
@@ -0,0 +1,64 @@
+const lsaOverview = [
+  {
+    id: 1,
+    question: "I am able to see things from the other person's point of view",
+  },
+  {
+    id: 2,
+    question: "I imagine how I would feel if I were in their place"
+  },
+  {
+    id: 3,
+    question: "I listen without needing to fix or dismiss someone's problems immediately",
+  },
+  {
+    id: 4,
+    question: "I operate my reasons for actions and decisions with my teams"
+  },
+  {
+    id: 5,
+    question: "I make sure that each person's voice is heard especially during difficult times"
+  },
+  {
+    id: 6,
+    question: "I create and maintain positive, reciprocal team relationships"
+  },
+  {
+    id: 7,
+    question: "I am the same person in all aspects of life - personal, work, community - with no need to present a different type of person in each situation"
+  },
+  {
+    id: 8,
+    question: "I maintain my values when it is easy and even when it is hard to do so"
+  },
+  {
+    id: 9,
+    question: "I speak directly even when the topic is uncomfortable but important",
+  },
+  {
+    id: 10,
+    question: "I pursue results with drive and energy"
+  },
+  {
+    id: 11,
+    question: "I set high standards for performance"
+  },
+  {
+    id: 12,
+    question: "I am quick to seize opportunities that broaden my skills and grow my influence" 
+  },
+  {
+    id: 13,
+    question: "I seek to learn new things for the joy of learning and growing"
+  },
+  {
+    id: 14,
+    question: "I look at operations that don't seem to be effective or efficient and try to figure out how to improve them"
+  },
+  {
+    id: 15,
+    question: "I want to understand why my boss makes decisions and choices, and has his/her opinions"
+  },
+];
+
+export default lsaOverview;
