7e1603f9c43dadaae170de79ee863cec9b5af114
Ivan <engr.ivan.evangelista@gmail.com>
2022-08-18 22:27:43 +0800

    add in leadership assessment screens to stack

--- app/navigators/AuthStack.js
+++ app/navigators/AuthStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SignIn, StartingLineScreen } from '../screens';
+import { LeadershipAssessment, LeadershipAssessmentGuide, SignIn, StartingLineScreen } from '../screens';
 
 const AuthStack = createStackNavigator();
 export default function AuthNavigator() {
@@ -15,9 +15,17 @@ export default function AuthNavigator() {
         component={StartingLineScreen}
       />
       <AuthStack.Screen
+        name={'Leadership Assessment'}
+        component={LeadershipAssessment}
+      />
+      <AuthStack.Screen
+        name={'Leadership Assessment Guide'}
+        component={LeadershipAssessmentGuide}
+      />
+      {/* <AuthStack.Screen
         name={'Sign in'}
         component={SignIn}
-      />
+      /> */}
     </AuthStack.Navigator>
   );
 }
