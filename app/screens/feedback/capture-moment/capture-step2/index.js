ea8b67026754fb61911691ec8a29c2913a986240
Ivan <engr.ivan.evangelista@gmail.com>
2022-08-22 16:14:55 +0800

    adjustments to capture fb moment step 2

--- app/screens/feedback/capture-moment/capture-step2/index.js
+++ app/screens/feedback/capture-moment/capture-step2/index.js
@@ -12,16 +12,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Ionicons';
+import CaptureMomentActions from 'app/store/CaptureFeedbackMomentRedux';
 import Images from 'app/assets/images';
 import styles from '../styles';
 
 
 const CaptureMomentStep2 = () => {
   const dispatch = useDispatch();
-  const [selection, setSelection] = useState({
+  const activeStep  = useSelector(state => state.captureMoment.get('activeStep'));
+  const [typeSelection, setTypeSelection] = useState({
     id: 0,
     icon: '',
     title: '',
+    hint: '',
   });
 
   const options = [
@@ -37,7 +40,15 @@ const CaptureMomentStep2 = () => {
       title: 'Corrective',
       hint: 'When you tell others what they need to improve on'
     }
-  ]
+  ];
+
+
+  const selectFeedbackType = () => {
+    dispatch(CaptureMomentActions.setCaptureData('step2', ))
+    setTimeout(() => {
+      dispatch(CaptureMomentActions.setCaptureActiveStep(activeStep + 1))
+    }, 200);
+  }
   return (
     <View style={styles.content}>
       <Text style={styles.mainQuestionText}>What kind of feedback do you want to give?</Text>
@@ -47,24 +58,28 @@ const CaptureMomentStep2 = () => {
           {options.map((item, index) => (
             <TouchableOpacity 
               key={item.id}
-              style={[styles.selectionButton, selection.id === item.id && styles.selectedButton]}
-              onPress={() => setSelection(item)}
+              style={[styles.selectionButton, typeSelection.id === item.id && styles.selectedButton]}
+              onPress={() => setTypeSelection(item)}
             >
               <Image
                 source={item.icon}
                 resizeMode="contain"
                 style={styles.selectionIcon}
               />
-              <Text style={[styles.selectionText, selection.id !== item.id && styles.unselectedText]}>{item.title}</Text>
+              <Text style={[styles.selectionText, typeSelection.id !== item.id && styles.unselectedText]}>{item.title}</Text>
             </TouchableOpacity>
           ))}
+        </View>
+        <View style={styles.hintContainer}>
+          <Text style={styles.hintText}>{typeSelection.hint}</Text>
         </View>
       </View>
       <View style={styles.btnContainer}>
         <Button
           mode='contained'
-          style={[styles.button, selection.id === 0 ? styles.disabledBtn : styles.enabledBtn]}
-        >Continue</Button>
+          onPress={() => selectFeedbackType()}
+          style={[styles.button, typeSelection.id === 0 ? styles.disabledBtn : styles.enabledBtn]}
+        ><Text style={{ color: typeSelection.id === 0 ? '#667070' : 'white' }}>Continue</Text></Button>
       </View>
       <View style={styles.spacer} />
     </View>
