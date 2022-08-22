40d8d444d099a0d8c1453dab5361673008b37598
Ivan <engr.ivan.evangelista@gmail.com>
2022-08-22 18:13:31 +0800

    adjustments to stack and routes

--- app/navigators/AuthStack.js
+++ app/navigators/AuthStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SignIn, SignUp, LeadershipAssessment, LeadershipAssessmentGuide, StartingLineScreen } from '../screens';
+import AssessmentNavigator from './AssessmentStack';
 
 const AuthStack = createStackNavigator();
 export default function AuthNavigator() {
@@ -14,6 +15,10 @@ export default function AuthNavigator() {
         name={'Starting line'}
         component={StartingLineScreen}
       />
+       <AuthStack.Screen
+            name={'Assessment'}
+            component={AssessmentNavigator}
+          />
       
       <AuthStack.Screen
         name={'Sign in'}
