a5a2c9561d62820f14f1ac4264b515a737c043b7
Ivan <engr.ivan.evangelista@gmail.com>
2022-08-23 15:43:11 +0800

    handle topics saga

--- app/sagas/CaptueMomentSaga.js
+++ app/sagas/CaptueMomentSaga.js
@@ -1,5 +1,5 @@
 import { checkInternetConnection } from 'react-native-offline';
-import { call, put, takeLatest, select } from 'redux/saga/effects';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
 import CaptureMomentActions, { CaptureMomentTypes } from 'app/store/CaptureFeedbackMomentRedux';
 import api from 'app/services/apiService';
 
@@ -12,30 +12,47 @@ export function* fetchLayerOneTopics() {
     } */
 
   const type = yield select(feedbackType);
+  
   console.warn(type);
+  const payloadType = type.title === 'Corrective' ? true : false;
   const payload ={
-      Corrective: '',
-      Positive: '',
+      Corrective: payloadType,
+      Positive: !payloadType,
     }
+
+  debugger;
   
   const response = yield call(api.getLayerOneTopics, payload);
+  debugger;
   if (response.ok) {
-
+    if (response.data.status === 'ok') {
+      const topicList = response.data.topics;
+      yield put(CaptureMomentActions.fetchLayerOneTopicsSuccess(topicList))
+    } else {
+      yield put(CaptureMomentActions.fetchLayerOneTopicsFailure(response.data));
+    }
+  } else {
+    yield put(CaptureMomentActions.fetchLayerOneTopicsFailure(response.data));
   }
 }
 
-export function* fetchLayerTwoTopics() {
+export function* fetchLayerTwoTopics( { data }) {
   /* const connected = yield checkInternetConnection();
     if (!connected) {
       return;
     } */
+    console.warn('saga', data);
   const payload ={
-    gen_topic_id: '',
+    gen_topic_id: data.id,
   }
   
   const response = yield call(api.getLayerTwoTopics, payload);
   if (response.ok) {
-
+    console.warn('res', response.data.subtopics);
+    if (response.data.status == 'ok') {
+      const layerTwo = response.data.subtopics;
+      yield put(CaptureMomentActions.fetchLayerTwoTopicsSuccess(layerTwo))
+    }
   }
 }
 
