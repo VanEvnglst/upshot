d91eb9ae74de82c3b745c4122ba42f578f2d5601
Ivan <engr.ivan.evangelista@gmail.com>
2022-09-28 20:34:52 +0800

    adjustments to gradient background component

--- app/components/gradient-background/index.js
+++ app/components/gradient-background/index.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 import LinearGradient from 'react-native-linear-gradient';
 
 const GradientBackground = props => {
-  const { children } = props;
+  const { children, colors, style } = props;
   return (
     <LinearGradient
-      colors={['#6200EE', '#BF38FF']}
-      style={styles.container}
+      colors={colors}
+      style={[styles.container, { ...style }]}
       start={{ x: 0.4, y: 0 }}
       end={{ x: 0.6, y: 1}}
     >
@@ -32,9 +32,11 @@ const styles = StyleSheet.create({
 
 GradientBackground.propTypes = {
   children: PropTypes.any,
+  colors: PropTypes.array
 };
 
 
 GradientBackground.defaultProps = {
-  children: {}
+  children: {},
+  colors: [],
 }
