725b156ecb629d8f5005cca6b9a4948c038d10c4
ravsrivera-upshot <raven@upshot.global>
2022-09-28 19:20:48 +0800

    Updated data naming

--- app/models/LSATypes.js
+++ app/models/LSATypes.js
@@ -3,35 +3,35 @@ const lsaTypes = [
   title: 'Achievement-Orientation',
   icon: "🏅",
   questionValue: 'achievementList',
-  categoryStateStat: 'achievementOrientation',
+  categoryState: 'achievementOrientation',
   color: "#7F88DB"
 },
 {
   title: 'Openness to Learn',
   icon: "🧠",
   questionValue: 'opennessToLearnList',
-  categoryStateStat: 'opennessToLearn',
+  categoryState: 'opennessToLearn',
   color: "#FBA262"
 },
 {
   title: 'Trust Building',
   icon: "🤝",
   questionValue: 'trustBuildingList',
-  categoryStateStat: 'trustBuilding',
+  categoryState: 'trustBuilding',
   color: "#D394EA"
  },
 {
   title: 'Empathy',
   icon: "💓",
   questionValue: 'empathyList',
-  categoryStateStat: 'empathy',
+  categoryState: 'empathy',
   color: "#64D3F6"
 },
 {
   title: 'Authenticity',
   icon: "👐",
   questionValue: 'authenticityList',
-  categoryStateStat: 'authenticity',
+  categoryState: 'authenticity',
   color: "#F06293"
 },
 
