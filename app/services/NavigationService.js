073348819838a1966bdc2f566a3751b58db961ea
Ivan <engr.ivan.evangelista@gmail.com>
2022-09-29 16:19:40 +0800

    create reset navigation

--- app/services/NavigationService.js
+++ app/services/NavigationService.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StackActions, CommonActions, NavigationActions, } from '@react-navigation/native';
 
 export const navigationRef = React.createRef();
 
@@ -9,3 +10,14 @@ export function navigate(name, params) {
   // }
 }
 
+export function navigateAndReset(routeName, params) {
+  navigationRef.current?.dispatch(
+    CommonActions.reset({
+      index: 0,
+      key: null,
+      routes: [
+        { name: routeName}
+      ]
+    })
+  );
+}
