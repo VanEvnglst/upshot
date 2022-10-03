32009361bea9a101f0ed6d3663391645719cd728
ravsrivera-upshot <raven@upshot.global>
2022-10-03 19:13:03 +0800

    Added Schedule Discussion screen

--- app/navigators/FeedbackStack.js
+++ app/navigators/FeedbackStack.js
@@ -28,6 +28,7 @@ import {
   RecordFeedbackEntry,
   EntryConfirmation,
   ReviewFeedbackEntry,
+  ScheduleDiscussion,
 } from '../screens';
 import { getActiveJourneys } from 'app/store/selectors';
 
@@ -54,7 +55,10 @@ export default function FeedbackNavigator() {
           name={'Record Feedback Entry'}
           component={RecordFeedbackEntry}
         />
-
+        <FeedbackStack.Screen
+          name={'Schedule Discussion'}
+          component={ScheduleDiscussion}
+        />
         <FeedbackStack.Screen
           name={'Feedback Entry Confirmation'}
           component={EntryConfirmation}
