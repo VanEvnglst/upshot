fc46b2cbd57b96ed76c80dc16a9e60079370a7b1
ravsrivera-upshot <raven@upshot.global>
2022-10-10 12:13:53 +0800

    Added Frontliner Assessment Model

---
+++ app/models/FrontlinerAssessmentModel.js
@@ -0,0 +1,39 @@
+export const AssessmentQuestions = [
+    {
+      //0
+      question: "Overall, how satisfied are you with the feedback Jasmine Diaz gave you?"
+    },
+    {
+      //1
+      question: "How did you feel after receiving feedback?"
+    },
+    {
+      //2
+      question: "My manager gave their feedback soon after the event occurred"
+    },
+    {
+      //3
+      question: "My manager gave specific information about the event that was observed"
+    },
+    {
+      //4
+      question: "My manager gave feedback in a calm, objective and non-judgmental manner"
+    },
+    {
+      //5
+      question: "My manager was clear with the next steps that I need to do"
+    },
+    {
+      //6
+      question: "My manager gave me very helpful feedback"
+    },
+    {
+      question: "My manager offered the kind of support that I requested "
+      //*Show only if FL answered this screen
+    },
+    {
+      question: "My manager clarified the questions I had ",
+      // *Show only if FL gave any feedback
+      // place shoud be after 5
+    },
+]
