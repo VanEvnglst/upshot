5de687ca1972ca02015664087d5236786e795de1
Ivan <engr.ivan.evangelista@gmail.com>
2022-10-12 19:43:54 +0800

    fix data being fetched from store

--- app/screens/feedback/response-exchange/exchange-confirmation/index.js
+++ app/screens/feedback/response-exchange/exchange-confirmation/index.js
@@ -19,7 +19,7 @@ const FeedbackExchangeConfirmation = props => {
   const { navigation } = props;
   const dispatch = useDispatch();
   const feedbackData = useSelector(getCurrentJourney);
-  const memberName = feedbackData.result.frontliner.split(' ');
+  const memberName = feedbackData.frontliner.split(' ');
   const memberInitials = `${memberName[0].charAt(0)}${memberName[1].charAt(
     0,
   )}`;
