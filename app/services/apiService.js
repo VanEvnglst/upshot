d23ab05afc78eabe9a5e0f10acfef783c5d8396b
ravsrivera-upshot <raven@upshot.global>
2022-10-12 11:17:28 +0800

    Added backend for FL Feedback Assessment

--- app/services/apiService.js
+++ app/services/apiService.js
@@ -499,6 +499,13 @@ export default {
     const uniqueId = await AsyncStorage.getItem('uniqueId');
 
     return upshotAPI.post(`/${uniqueId}/schedule_face_to_face`, payload);
-  }
+  },
 
+  postRecordingFLAssessment: async payload => { 
+    const url = await AsyncStorage.getItem('baseURL');
+    const upshotAPI = setAPI(url);
+    const uniqueId = await AsyncStorage.getItem('uniqueId');
+
+    return upshotAPI.post(`/${uniqueId}/fl_fb_assessment`, payload);
+  }
 };
