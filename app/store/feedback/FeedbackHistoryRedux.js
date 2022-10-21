4ebe1e03683bfb65b3e120835a696ccc05cf4cea
Ivan <engr.ivan.evangelista@gmail.com>
2022-10-21 15:23:22 +0800

    handle user activity data from api

--- app/store/feedback/FeedbackHistoryRedux.js
+++ app/store/feedback/FeedbackHistoryRedux.js
@@ -6,6 +6,12 @@ export const INITIAL_STATE = Map({
   fetching: false,
   error: null,
   activeJourneyList: [],
+  assessmentProgress: '',
+  upcomingDiscussions: [],
+  totalJourneys: null,
+  scheduledDiscussions: null,
+  ongoingJourneys: null,
+  completedJourneys: null,
   recentJourneys: [],
 });
 
@@ -17,6 +23,9 @@ const { Types, Creators } = createActions({
   fetchActiveJourneys: [''],
   fetchActiveJourneysSuccess: ['activeJourneyList'],
   fetchActiveJourneysFailure: ['error'],
+  fetchUserActivity: [''],
+  fetchUserActivitySuccess: ['userActivity'],
+  fetchUserActivityFailure: ['error'],
 });
 
 export const FeedbackHistoryTypes = Types;
@@ -65,6 +74,33 @@ const fetchRecentJourneysFailure = (state, { error }) => {
   });
 };
 
+const fetchUserActivity = state =>
+  state.merge({
+    fetching: true,
+    error: '',
+  });
+
+const fetchUserActivitySuccess = (state, { userActivity }) => {
+  const {assessmentProgress, active_journeys, upcoming_one_on_ones, ongoing_journeys_count, scheduled_discussions_count, completed_journeys_count } = userActivity;
+
+  return state.merge({
+    fetching: false,
+    assessmentProgress: assessmentProgress,
+    activeJourneyList: active_journeys,
+    upcomingDiscussions: upcoming_one_on_ones,
+    ongoingJourneys: ongoing_journeys_count,
+    scheduledDiscussions: scheduled_discussions_count,
+    completedJourneys: completed_journeys_count,
+    totalJourneys: ongoing_journeys_count + scheduled_discussions_count + completed_journeys_count,
+  });
+}
+
+const fetchUserActivityFailure = (state, { error }) =>
+  state.merge({
+    fetching: false,
+    error
+  });
+
 /* ------------- Hookup Reducers To Types ------------- */
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.FETCH_ACTIVE_JOURNEYS]: fetchActiveJourneys,
@@ -73,4 +109,7 @@ export const reducer = createReducer(INITIAL_STATE, {
   [Types.FETCH_RECENT_JOURNEYS]: fetchRecentJourneys,
   [Types.FETCH_RECENT_JOURNEYS_SUCCESS]: fetchRecentJourneysSuccess,
   [Types.FETCH_RECENT_JOURNEYS_FAILURE]: fetchRecentJourneysFailure,
+  [Types.FETCH_USER_ACTIVITY]: fetchUserActivity,
+  [Types.FETCH_USER_ACTIVITY_SUCCESS]: fetchUserActivitySuccess,
+  [Types.FETCH_USER_ACTIVITY_FAILURE]: fetchUserActivityFailure,
 });
