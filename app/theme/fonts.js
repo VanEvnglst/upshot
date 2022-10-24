3dc88fcb38829c73d64e1cc410da878e6872ff4f
Ivan <engr.ivan.evangelista@gmail.com>
2022-10-24 23:15:45 +0800

    update font sizes and styles

--- app/theme/fonts.js
+++ app/theme/fonts.js
@@ -1,30 +1,44 @@
 const size = {
-  h1: 96,
-  h2: 60,
-  h3: 48,
-  h4: 34,
-  h5: 24,
-  h6: 20,
-  body1: 16,
-  body2: 14,
-  caption: 12,
-  overline: 10,
+  hero: 96,
+  h1: 64,
+  h2: 48,
+  h3: 40,
+  h4: 32,    
+  body1: 24,
+  body2: 16,
+  caption1: 14,
+  caption2: 12,
+  caption3: 10,
 };
 
 const letterSpacing = {
-  h1: -15,
-  h2: -8,
-  h3: 0,
-  h4: 0,
-  h5: 0.075,
-  h6: 0.075,
-  subtitle1: 0.09,
-  subtitle2: 0.07,
-  button: 0.89,
-  body1: 0.31,
-  body2: 0.18,
-  caption: 0.33,
-  overline: 1.5,
+  hero: '-0.02em',
+  h1: '-0.02em',
+  h2: '-0.02em',
+  h3: '-0.01em',
+  h4: '-0.01em',
+};
+
+const lineHeight = {
+  hero: 96,
+  h1: 64,
+  h2: 56,
+  h3: 48,
+  h4: 40,
+  body1: 32,
+  body2: 24,
+  caption1: 24,
+  caption2: 20,
+  caption3: 16,
+  hairline: 12,
+};
+
+
+const fontWeight = {
+  regular: '400',
+  medium: '500',
+  semiBold: '600',
+  bold: '700'
 };
 
 const fontFamily = {
@@ -41,4 +55,6 @@ export default {
   fontFamily,
   size,
   letterSpacing,
+  lineHeight,
+  fontWeight,
 };
