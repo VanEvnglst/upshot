cda047516b83a3914e21d41b15433ce09a9f3237
Ivan <engr.ivan.evangelista@gmail.com>
2022-11-08 15:54:44 +0800

    update sign up container usage

--- app/screens/starting-line/index.js
+++ app/screens/starting-line/index.js
@@ -1,233 +1,82 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import {
   View,
   TouchableOpacity,
-  KeyboardAvoidingView,
   Dimensions,
 } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
-import BottomSheet, { BottomSheetTextInput } from '@gorhom/bottom-sheet';
+import BottomSheet from '@gorhom/bottom-sheet';
 import PropTypes from 'prop-types';
-import { Formik } from 'formik';
-import { getSignInError } from 'app/store/selectors';
 import { Text } from 'app/components';
-import { InputUtil, DeviceUtil } from 'app/utils';
-import labels from 'app/locales/en';
 import styles from './styles';
 import SignIn from '../sign-in';
+import SignUp from '../sign-up';
 
 const StartingLineScreen = props => {
   const { navigation } = props;
   const bottomSheetRef = useRef(null);
-  const dispatch = useDispatch();
   const { height } = Dimensions.get('window');
   const snapPoints = useMemo(() => ['95%'], []);
-  const signInError = useSelector(getSignInError);
   const [sheetType, setSheetType] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-
-  const validate = () => {
-    const errors = {};
-    if (email.length === 0) {
-      errors.emailError = labels.errors.emailRequired;
-    }
-
-    if (password.length === 0) {
-      errors.passwordError = labels.errors.passwordRequired;
-    }
-
-    if (email && email.length !== 0 && !InputUtil.validateEmail(email)) {
-      errors.emailError = labels.errors.validEmail;
-    }
-
-    return errors;
-  };
-
-  const signInUser = () => {
-    console.warn('call sign in')
-    // dispatch(AuthenticationActions.fetchServer({
-    //   email,
-    //   password,
-    // }))
-  };
-
-  const signUpUser = () => {
-    console.warn('call sign up');
-    navigation.replace('Leadership Assessment Guide')
-  }
 
   const openSheet = type => {
-    console.warn('call');
     setSheetType(type);
     bottomSheetRef.current?.snapToIndex(0);
   };
 
-  const InputField = props => {
-    const {
-      label,
-      placeholder,
-      error,
-      type,
-      value
-    } = props;
-    return (
-      <View style={{ marginBottom: 24,}}>
-        <Text style={{ marginBottom: 4 }}>{label}</Text>
-        <BottomSheetTextInput
-          {...props}
-          // secureTextEntry={type==='password'}
-          placeholder={placeholder}
-          style={{
-            borderWidth: 1,
-            borderColor: '#667080',
-            height: 48,
-            paddingHorizontal: 6,
-            borderRadius: 6,
-          }}
-        />
-        {error && error.length !== 0 && <Text>{error}</Text>}
-      </View>
-    );
-  };
+  const closeSheet = () => {
+    setSheetType('');
+    bottomSheetRef.current?.close();
+  }
 
-  const SignUpContainer = () => {
-    return (
-      <>
-          <Formik
-            initialValues={{
-              email,
-              password,
-              name,
-            }}
-            // validate={() => validate()}
-            onSubmit={() => signUpUser()}>
-            {({ errors, handleSubmit }) => {
-              return (
-                <>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      justifyContent: 'space-between',
-                      alignItems: 'center',
-                      height: '25%',
-                      paddingLeft: 18,
-                      paddingRight: 12,
-                      borderBottomWidth: 1,
-                    }}>
-                    <TouchableOpacity>
-                      <Text>X</Text>
-                    </TouchableOpacity>
-                    <Text
-                      style={{
-                        fontSize: 16,
-                        lineHeight: 22,
-                        fontWeight: '700',
-                        paddingLeft: 30,
-                      }}>
-                      Sign up
-                    </Text>
-                    <TouchableOpacity onPress={() => setSheetType('sign in')}>
-                      <Text>Log in</Text>
-                    </TouchableOpacity>
-                  </View>
-                  <View style={{ paddingHorizontal: 22, marginTop: 30 }}>
-                    <InputField
-                      label={'Name'}
-                      placeholder={'John Doe'}
-                      autoFocus
-                      value={name}
-                      onChangeText={name => setName(name)}
-                      error={errors.nameError}
-                    />
-                    <InputField
-                      label={'Email'}
-                      placeholder={'name@work-email.com'}
-                      value={email}
-                      onChangeText={email => setEmail(email)}
-                      error={errors.emailError}
-                    />
-                    <InputField
-                      label={'Password'}
-                      placeholder={'8+ characters'}
-                      secureTextEntry
-                      value={password}
-                      onChangeText={password => setPassword(password)}
-                      error={errors.passwordError}
-                    />
-                    <Button
-                      mode="contained"
-                      style={{
-                        marginTop: 30,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: 58,
-                      }}
-                      onPress={() => handleSubmit()}  
-                    >
-                      <Text>Sign up</Text>
-                    </Button>
-                  </View>
-                </>
-              );
-            }}
-          </Formik>
-      </>
-    );
-  };
+  const handleSheets = () => {
+    if(sheetType === 'sign in')
+      setSheetType('sign up')
+    else
+      setSheetType('sign in')
+  }
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <View
-        style={{ flex: 1, marginTop: 70, paddingLeft: 25, maxWidth: '70%' }}>
+        style={styles.header}>
         <Text
-          style={{
-            fontSize: 32,
-            lineHeight: 36,
-            color: '#667080',
-            opacity: 0.7,
-            fontWeight: '700',
-          }}>
+          type='h4'
+          weight='bold'
+          style={styles.headerTitle}>
           What if you can track your own
         </Text>
         <Text
-          style={{
-            fontSize: 32,
-            lineHeight: 36,
-            color: '#667080',
-            fontWeight: '700',
-          }}>
+          type='h4'
+          weight='bold'
+          style={styles.headerTitleDark}>
           {'leadership\nperformance?'}
         </Text>
       </View>
       <View style={{ flex: 2 }} />
       <View
-        style={{
-          flex: 1,
-          paddingLeft: 25,
-          paddingRight: 18,
-          marginBottom: 15,
-        }}>
+        style={styles.actionContainer}>
         <Button
           mode="contained"
           onPress={() => navigation.navigate('Starting guide')}
           style={styles.button}>
-          <Text>tell me more</Text>
+          <Text type='body2' weight='bold' style={styles.tellMeMoreText}>tell me more</Text>
         </Button>
         <Button
           mode="contained"
-          onPress={() => navigation.navigate('Sign up')}
+          onPress={() => openSheet('sign up')}
           style={[styles.button, styles.newAccountButton]}>
-          <Text style={{ color: 'black' }}>Sign up</Text>
+          <Text 
+            type='body2'
+            weight='bold'
+          style={styles.signUpBtnText}>Sign up</Text>
         </Button>
       </View>
       <View style={styles.oldAccountContainer}>
-        <Text>Already have an account?</Text>
+        <Text type='caption1' weight='regular' style={styles.labelsText}>Already have an account?</Text>
         <TouchableOpacity onPress={() => openSheet('sign in')}>
-          <Text style={styles.logInText}>
+          <Text type='caption1' weight='regular' style={styles.logInText}>
             Log in
           </Text>
         </TouchableOpacity>
@@ -243,7 +92,7 @@ const StartingLineScreen = props => {
           <View style={styles.bottomSheetHeader}>
             <TouchableOpacity
               accessibilityRole='button'
-              onPress={() => {}}
+              onPress={() => closeSheet()}
             >
               <Icon name='close-outline'
                 size={24}
@@ -254,13 +103,13 @@ const StartingLineScreen = props => {
             <Text type='body2' weight='bold' style={styles.bottomSheetTitle}>{sheetType === 'sign in' ? 'Log In' : 'Sign Up'}</Text>
             <TouchableOpacity
               accessibilityRole='button'
-              onPress={() => {}}
+              onPress={() => handleSheets()}
             >
             <Text type='body2' weight='medium' style={styles.bottomSheetOption}>{sheetType === 'sign in' ? 'Sign Up' : 'Log In'}</Text>
             </TouchableOpacity>
           </View>
            {sheetType === 'sign in' && <SignIn/>}
-          {/* {sheetType === 'sign up' && <SignUpContainer />} */}
+          {sheetType === 'sign up' && <SignUp/>}
         </View>
       </BottomSheet>
       <View style={styles.spacer} />
@@ -269,3 +118,11 @@ const StartingLineScreen = props => {
 };
 
 export default StartingLineScreen;
+
+StartingLineScreen.propTypes = {
+  navigation: PropTypes.object,
+};
+
+StartingLineScreen.defaultProps = {
+  navigation: {},
+}
--- app/screens/starting-line/styles.js
+++ app/screens/starting-line/styles.js
@@ -6,32 +6,53 @@ const { normalize } = DeviceUtil;
 
 export default styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+    marginTop: 70,
+    paddingLeft: 25,
+    maxWidth: '70%',
+  },
+  headerTitle: {
+    color: '#667080',
+    opacity: 0.7,
+  },
+  headerTitleDark: {
+    color: '#667080',
+  },
+  actionContainer: {
+    flex: 1,
+    paddingLeft: 25,
+    paddingRight: 18,
+    marginBottom: 15,
   },
   button: {
     height: 55,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: Colors.neutral3,
+    backgroundColor: Colors.neutral4,
   },
   newAccountButton: {
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 8,
-            borderWidth: 1,
+    borderWidth: 2,
+    borderColor: Colors.neutral4,
   },
   oldAccountContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'center' 
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   logInText: {
-    textDecorationLine: 'underline', 
-    marginLeft: 4
+    textDecorationLine: 'underline',
+    marginLeft: 4,
+    color: Colors.neutral4,
   },
   bottomSheetHeader: {
     flexDirection: 'row',
-    justifyContent:'space-between',
+    justifyContent: 'space-between',
     paddingHorizontal: normalize(18),
     borderBottomWidth: 1,
     borderBottomColor: Colors.neutral5,
@@ -41,15 +62,25 @@ export default styles = StyleSheet.create({
     paddingLeft: normalize(40),
     flex: 3,
     textAlign: 'center',
-    color: Colors.neutral3
+    color: Colors.neutral3,
   },
   bottomSheetOption: {
-    flex: 1, 
+    flex: 1,
     textAlign: 'right',
-    color: Colors.neutral3
+    color: Colors.neutral3,
+  },
+  signUpBtnText: {
+    color: Colors.neutral4,
+    textTransform: 'capitalize',
+  },
+  tellMeMoreText: {
+    color: Colors.white,
+    textTransform: 'capitalize',
+  },
+  labelsText: {
+    color: Colors.neutral4,
   },
   spacer: {
-    height: 100
-  }
+    height: 100,
+  },
 });
-
