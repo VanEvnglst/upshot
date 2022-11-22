cafbddf1aefabb58298cf5f20d62b596e3f2f2fe
Ivan <engr.ivan.evangelista@gmail.com>
2022-11-22 08:15:34 +0800

    create data utils file

---
+++ app/utils/DataUtil.js
@@ -0,0 +1,19 @@
+
+function parseInitials(name) {
+  const nameArr = name.split(' ');
+  const nameLength = nameArr.length;
+  let initials = '';
+  if(nameLength === 1){
+    initials = `${nameArr[0].charAt(0)}`;
+  } else if (nameLength === 2) {
+    initials = `${nameArr[0].charAt(0)}${nameArr[1].charAt(0)}`;
+  } else {
+    initials = `${nameArr[0].charAt(0)}${nameArr[nameLength - 1].charAt(0)}`;
+  }
+  return initials;
+}
+
+
+export default {
+  parseInitials
+}
