5faf2713569f6ec0cb117f6f2237d21c07c11a55
Ivan <engr.ivan.evangelista@gmail.com>
2022-12-13 20:26:18 +0800

    updates to story progress component

--- app/components/story-progress/index.js
+++ app/components/story-progress/index.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 import styles from './styles';
 
 const StoryProgress = props => {
-  const { length, activeStep } = props;
+  const { length, activeStep, type } = props;
   const ITEM_SIZE = 20;
   return (
     <View style={styles.container}>
     {Array.apply(null, { length: length}).map((item, i) => (
       <View
         key={i}
-        style={[styles.stepContainer, i + 1 <= activeStep ? styles.activeStep : styles.inactiveStep, { width: ITEM_SIZE + (2 * length) }]}
+        style={[
+          styles.stepContainer, 
+          i + 1 <= activeStep ? type === 'light' ? styles.lightActiveStep : styles.activeStep : type === 'light' ? styles.lightInactiveStep : styles.inactiveStep, { width: ITEM_SIZE + (3 * length) }]}
       />
     ))}
     </View>
@@ -22,10 +24,12 @@ export default StoryProgress;
 
 StoryProgress.propTypes = {
   length: PropTypes.number.isRequired,
-  activeStep: PropTypes.number.isRequired
+  activeStep: PropTypes.number.isRequired,
+  type: PropTypes.string,
 };
 
 StoryProgress.defaultProps = {
   length: 4,
-  activeStep: 1
+  activeStep: 1,
+  type: 'dark'
 }
