72988c9b8be0323ace276d9ab9149c144dbcbf57
Ivan <engr.ivan.evangelista@gmail.com>
2022-12-30 15:01:59 +0800

    updates to leadership break down screen

--- app/screens/leadership-assessment/lsa-assessment-break-down/index.js
+++ app/screens/leadership-assessment/lsa-assessment-break-down/index.js
@@ -5,21 +5,25 @@ import {
   BackHandler,
   Image,
   TouchableOpacity,
-  Text,
-  ScrollView
+  ScrollView,
 } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Ionicons';
 import LeadershipSkillAreaActions from 'app/store/LSARedux';
-import { getUserName, getCompletedCount, getExtendedTestResults, getCategoryStatus } from 'app/store/selectors';
+import {
+  getUserName,
+  getCompletedCount,
+  getExtendedTestResults,
+  getCategoryStatus,
+} from 'app/store/selectors';
+import { Text } from 'app/components';
 import lsaTypes from 'app/models/LSATypes';
 import Images from 'app/assets/images';
 import styles from './styles';
 
-
 const AssessmentBreakDown = props => {
-  const { navigation } = props;
+  const { navigation, route } = props;
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -42,108 +46,110 @@ const AssessmentBreakDown = props => {
   const checkCategoryStatus = useSelector(getCategoryStatus);
   const completedCnt = useSelector(getCompletedCount);
 
-  const retrieveExtendedQs = async (element) => {
+  const retrieveExtendedQs = async element => {
     await dispatch(LeadershipSkillAreaActions.fetchExtendedQuestions());
-      setTimeout(() => {
-        navigation.navigate('Assessment', {screen: 'Leadership Assessment Extended', params: element });
+    setTimeout(() => {
+      navigation.navigate('Assessment', {
+        screen: 'Leadership Assessment Extended',
+        params: element,
+      });
     }, 300);
-    
   };
 
-  const SkillAreaItem = ({element}) => {
+  const SkillAreaItem = ({ element }) => {
     return (
       <>
-        {checkCategoryStatus[element.categoryState] - 3 === 7 ?
+        {checkCategoryStatus[element.categoryState] - 3 === 7 ? (
           <TouchableOpacity
-            accessibilityRole='button'
+            accessibilityRole="button"
             style={styles.skillAreaItem}
-            //onPress={() => navigation.navigate('Leadership Assessment Extended', { category: title, dataValue: value })}  
-            
+            //onPress={() => navigation.navigate('Leadership Assessment Extended', { category: title, dataValue: value })}
           >
-            <View style={[styles.stepCounter, {borderColor: element.color}]}>
-              <Text style={[styles.stepText, { color: element.color, }]}>{checkCategoryStatus[element.categoryState]}/10</Text>
+            <View style={[styles.stepCounter, { borderColor: element.color }]}>
+              <Text style={[styles.stepText, { color: element.color }]}>
+                {checkCategoryStatus[element.categoryState]}/10
+              </Text>
             </View>
             <View style={styles.skillTitleContainer}>
-              <Text style={styles.skillTitleText}>{element.title} {element.icon}</Text>
+              <Text style={styles.skillTitleText}>
+                {element.title} {element.icon}
+              </Text>
               <View style={styles.completedContainer}>
                 <Text style={styles.completedText}>100% completed</Text>
               </View>
             </View>
           </TouchableOpacity>
-          :
+        ) : (
           <TouchableOpacity
-            accessibilityRole='button'
+            accessibilityRole="button"
             style={styles.skillAreaItem}
-            //onPress={() => navigation.navigate('Leadership Assessment Extended', { category: title, dataValue: value })}  
-            onPress={() => retrieveExtendedQs(element)}
-          >
+            //onPress={() => navigation.navigate('Leadership Assessment Extended', { category: title, dataValue: value })}
+            onPress={() => retrieveExtendedQs(element)}>
             <View style={[styles.stepCounter, { borderColor: '#BAC0CA' }]}>
-                <Text style={[styles.stepText, { color: '#667080', }]}> {checkCategoryStatus[element.categoryState] === null ? 0 : checkCategoryStatus[element.categoryState]}/10</Text>
+              <Text style={[styles.stepText, { color: '#667080' }]}>
+                {' '}
+                {checkCategoryStatus[element.categoryState] === null
+                  ? 0
+                  : checkCategoryStatus[element.categoryState]}
+                /10
+              </Text>
             </View>
             <View style={styles.skillTitleContainer}>
-              <Text style={styles.skillTitleText}>{element.title} {element.icon}</Text>
-              <Text style={styles.completionText}>{(checkCategoryStatus[element.categoryState]/10)*100}% completed</Text>
+              <Text style={styles.skillTitleText}>
+                {element.title} {element.icon}
+              </Text>
+              <Text style={styles.completionText}>
+                {(checkCategoryStatus[element.categoryState] / 10) * 100}%
+                completed
+              </Text>
             </View>
-            <View
-              accessibilityRole='button'
-            >
-              <Icon
-                name='chevron-forward-outline'
-                size={24}
-              />
+            <View accessibilityRole="button">
+              <Icon name="chevron-forward-outline" size={24} />
             </View>
           </TouchableOpacity>
-        }
-        </>
-      )
+        )}
+      </>
+    );
+  };
 
-    }
-
-
-  return <SafeAreaView style={styles.container}>
-    <ScrollView>
-    <View style={styles.headerContainer}>
-      <TouchableOpacity
-        accessibilityRole='button'
-        onPress={() => navigation.goBack()}
-        style={styles.icon}
-      >
-        <Icon
-          name='chevron-back-outline'
-          size={24}
-        />
-      </TouchableOpacity>
-      <View style={styles.headerTextContainer}>
-        <Text style={styles.headerText}>
-          Leadership Skill Area
-        </Text>
-      </View>
-      <View style={styles.horizontalSpacer} />
-    </View>
-    <View style={styles.titleContainer}>
-      <View style={styles.titleUser}>
-        <View style={{ flexDirection: 'row', alignItems: 'center'}}>
-        <Text style={styles.levelText}>Let's get started </Text>
-          <Image source={Images.bigGrinEmoji} style={{ height: 16, width: 16 }} />
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity
+            accessibilityRole="button"
+            onPress={() => navigation.goBack()}
+            style={styles.icon}>
+            <Icon name="chevron-back-outline" size={24} />
+          </TouchableOpacity>
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText}>Leadership Skill Area</Text>
           </View>
-        <Text style={styles.descriptionText}>Your Upshot journey awaits. With a little jump start, you will see where your strength lies by answering these areas.</Text>
-        <Text style={styles.completedLabelText}>Completed Areas</Text>
-        <Text style={styles.completedTestText}>{ completedCnt }/5</Text>
-      </View>
-      {/* <View style={styles.ringsContainer}/> */}
-    </View>
-    <View style={styles.contentContainer}>
-      
-    {lsaTypes.map(element => {
-
-return (
-  <SkillAreaItem
-        element={element}
-      />
-);
-
-    })}
-        {/* {completedCnt === 5 ?
+          <View style={styles.horizontalSpacer} />
+        </View>
+        <View style={styles.titleContainer}>
+          <View style={styles.titleUser}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={styles.levelText}>Let's get started </Text>
+              <Image
+                source={Images.bigGrinEmoji}
+                style={{ height: 16, width: 16 }}
+              />
+            </View>
+            <Text style={styles.descriptionText}>
+              Your Upshot journey awaits. With a little jump start, you will see
+              where your strength lies by answering these areas.
+            </Text>
+            <Text style={styles.completedLabelText}>Completed Areas</Text>
+            <Text style={styles.completedTestText}>{completedCnt}/5</Text>
+          </View>
+          {/* <View style={styles.ringsContainer}/> */}
+        </View>
+        <View style={styles.contentContainer}>
+          {lsaTypes.map(element => {
+            return <SkillAreaItem key={element.id} element={element} />;
+          })}
+          {/* {completedCnt === 5 ?
           <TouchableOpacity
             style={{ marginTop: 36, backgroundColor: '#667080', height: 48, justifyContent: 'center', alignItems: 'center', width: '100%', borderRadius: 6 }}
             onPress={() => navigation.navigate('Assessment', { screen: 'Calculate Assessment Score'})}>
@@ -154,14 +160,30 @@ return (
             <Text style={{ fontSize: 16, lineHeight: 22, fontWeight: '700', color: '#66708080' }}>Recalculate Indicator Levels</Text>
           </View>
         } */}
-        <TouchableOpacity style={{ marginTop: 10, justifyContent: 'center', alignItems: 'center' }}
-          onPress={() => navigation.navigate('Home')}>
-          <Text style={{ fontSize: 16, lineHeight: 22, fontWeight: '400', color: '#667080' }}>Skip for Later</Text>
-        </TouchableOpacity>
-        <View style={{height: 50}} />
-    </View>
-    </ScrollView>
-  </SafeAreaView>
+          {route.params.root === 'home' ? null : (
+            <TouchableOpacity
+              style={{
+                marginTop: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              onPress={() => navigation.navigate('Home')}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  lineHeight: 22,
+                  fontWeight: '400',
+                  color: '#667080',
+                }}>
+                Skip for Later
+              </Text>
+            </TouchableOpacity>
+          )}
+          <View style={{ height: 100 }} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default AssessmentBreakDown;
