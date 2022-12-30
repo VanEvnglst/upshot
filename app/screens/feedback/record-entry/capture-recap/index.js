f76d57dafe3abe61756dc8502ec21e91abbf61bb
Ivan <engr.ivan.evangelista@gmail.com>
2022-12-30 15:30:59 +0800

    updates to record entry screen

--- app/screens/feedback/record-entry/capture-recap/index.js
+++ app/screens/feedback/record-entry/capture-recap/index.js
@@ -15,14 +15,15 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
-import CaptureMomentActions from 'app/store/CaptureFeedbackMomentRedux';
+import CaptureMomentActions from 'app/store/feedback/CaptureFeedbackMomentRedux';
 import Images from 'app/assets/images';
 
-
-const CaptureFeedbackRecap = props => { 
+const CaptureFeedbackRecap = props => {
   const { navigation } = props;
   const dispatch = useDispatch();
-  const activeStep = useSelector(state => state.captureMoment.get('entryActiveStep'));
+  const activeStep = useSelector(state =>
+    state.captureMoment.get('entryActiveStep'),
+  );
   const maxStep = useSelector(state => state.captureMoment.get('entryMaxStep'));
   const feedbackData = useSelector(state => state.captureMoment.get('data'));
   const dateLogged = useSelector(
@@ -32,57 +33,143 @@ const CaptureFeedbackRecap = props => {
   return (
     <ScrollView style={styles.container}>
       <SafeAreaView>
-      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', borderBottomWidth: 1, borderBottomColor: "#B1B5C3", paddingBottom: 20}}>
-        <TouchableOpacity
-          accessibilityRole="button"
-          onPress={() => handleGoBack()}
-          style={{paddingLeft: 24, color: '#667080'}}>
-          <Icon name="chevron-back-outline" size={24} />
-        </TouchableOpacity>
-        <View>
-              <Text style={{fontSize: 24, lineHeight: 30, fontWeight: '700', color: '#667080', paddingTop: 20}}>Feedback Summary</Text>
-              <Text style={styles.selectedName}>Review the details before proceeding</Text>
-            </View>
-            </View>
-        <View style={styles.contentContainer}>
-        <View style={{marginBottom: 12, flexDirection: 'row'}}>
-          <View style={{ width: 24, height: 24, borderRadius: 32, backgroundColor: '#667080', opacity: 0.5, marginRight: 12}}/>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            borderBottomWidth: 1,
+            borderBottomColor: '#B1B5C3',
+            paddingBottom: 20,
+          }}>
+          <TouchableOpacity
+            accessibilityRole="button"
+            onPress={() => handleGoBack()}
+            style={{ paddingLeft: 24, color: '#667080' }}>
+            <Icon name="chevron-back-outline" size={24} />
+          </TouchableOpacity>
           <View>
-            <Text style={{ fontSize: 16, marginBottom: 4, color: '#667080', fontWeight: '700', lineHeight: 22}}>{feedbackData.step1.name}</Text>
-            <Text style={{ fontSize: 14, lineHeight: 22, color: '#667080', fontWeight: '400'}}>{dateLogged}</Text>
+            <Text
+              style={{
+                fontSize: 24,
+                lineHeight: 30,
+                fontWeight: '700',
+                color: '#667080',
+                paddingTop: 20,
+              }}>
+              Feedback Summary
+            </Text>
+            <Text style={styles.selectedName}>
+              Review the details before proceeding
+            </Text>
           </View>
+        </View>
+        <View style={styles.contentContainer}>
+          <View style={{ marginBottom: 12, flexDirection: 'row' }}>
+            <View
+              style={{
+                width: 24,
+                height: 24,
+                borderRadius: 32,
+                backgroundColor: '#667080',
+                opacity: 0.5,
+                marginRight: 12,
+              }}
+            />
+            <View>
+              <Text
+                style={{
+                  fontSize: 16,
+                  marginBottom: 4,
+                  color: '#667080',
+                  fontWeight: '700',
+                  lineHeight: 22,
+                }}>
+                {feedbackData.step1.name}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  lineHeight: 22,
+                  color: '#667080',
+                  fontWeight: '400',
+                }}>
+                {dateLogged}
+              </Text>
+            </View>
           </View>
           <View style={styles.topicContainer}>
-          <Text style={[styles.topicLabel, { marginBottom: 4, marginTop: 12 }]}>Feedback Type</Text>
-        <View style={styles.topicPicker}>
-          <Image
-            source={feedbackData.step2.title === 'Corrective' ? Images.penEmoji : Images.redHeartEmoji}
-            resizeMode='contain'
-            style={{ width: 24, height: 24, marginRight: 8}}
-          />
-          <Text style={styles.topicLabel}>{feedbackData.step2.title} Feedback</Text>
-        </View>
-        
-        <Text style={[styles.topicLabel, { marginBottom: 4, marginTop: 12 }]}>Topic</Text>
-        <View style={styles.topicPicker}>
-              <Text style={styles.topicLabel}>{topics.selectedLayerOne.name}</Text>
+            <Text
+              style={[styles.topicLabel, { marginBottom: 4, marginTop: 12 }]}>
+              Feedback Type
+            </Text>
+            <View style={styles.topicPicker}>
+              <Image
+                source={
+                  feedbackData.step2.title === 'Corrective'
+                    ? Images.penEmoji
+                    : Images.redHeartEmoji
+                }
+                resizeMode="contain"
+                style={{ width: 24, height: 24, marginRight: 8 }}
+              />
+              <Text style={styles.topicLabel}>
+                {feedbackData.step2.title} Feedback
+              </Text>
             </View>
-            <Text style={[styles.topicLabel, { marginBottom: 4, marginTop: 12}]}>Sub-topic</Text>
-        <View style={styles.topicPicker}>
-              <Text style={styles.topicLabel}>{topics.selectedLayerTwo.name}</Text>
+
+            <Text
+              style={[styles.topicLabel, { marginBottom: 4, marginTop: 12 }]}>
+              Topic
+            </Text>
+            <View style={styles.topicPicker}>
+              <Text style={styles.topicLabel}>
+                {topics.selectedLayerOne.name}
+              </Text>
             </View>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: '70%'}}>
-              <TouchableOpacity style={{ minWidth: '95%', backgroundColor: '#667080', height: 48, borderRadius: 6, justifyContent: 'center', alignItems: 'center' }}
+            <Text
+              style={[styles.topicLabel, { marginBottom: 4, marginTop: 12 }]}>
+              Sub-topic
+            </Text>
+            <View style={styles.topicPicker}>
+              <Text style={styles.topicLabel}>
+                {topics.selectedLayerTwo.name}
+              </Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: '70%',
+              }}>
+              <TouchableOpacity
+                style={{
+                  minWidth: '95%',
+                  backgroundColor: '#667080',
+                  height: 48,
+                  borderRadius: 6,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
                 onPress={() => navigation.navigate('Record Feedback Entry')}>
-                <Text style={{fontSize: 16, lineHeight: 22, fontWeight: '700', color: '#FFFFFF'}}>Continue</Text>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    lineHeight: 22,
+                    fontWeight: '700',
+                    color: '#FFFFFF',
+                  }}>
+                  Continue
+                </Text>
               </TouchableOpacity>
             </View>
           </View>
-          </View>
+        </View>
       </SafeAreaView>
-      </ScrollView>
-  )
-}
+    </ScrollView>
+  );
+};
 
 export default CaptureFeedbackRecap;
 
@@ -197,8 +284,6 @@ const styles = StyleSheet.create({
     color: '#667080',
   },
 
-
-
   topicContainer: {
     flex: 1,
   },
@@ -230,7 +315,7 @@ const styles = StyleSheet.create({
     color: '#5495EF',
     fontSize: 12,
     lineHeight: 18,
-    fontWeight: '400'
+    fontWeight: '400',
   },
   entriesContainer: {
     marginTop: 24,
@@ -250,6 +335,6 @@ const styles = StyleSheet.create({
     lineHeight: 22,
     fontWeight: '400',
     color: '#667080',
-    opacity: 0.7
-  }
+    opacity: 0.7,
+  },
 });
