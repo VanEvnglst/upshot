1db0d0540274b2fb3bfe0c664b8c52f9de8ce4c5
Ivan <engr.ivan.evangelista@gmail.com>
2022-12-30 14:32:12 +0800

    code cleanup for feedback history

--- app/sagas/feedback/FeedbackHistorySaga.js
+++ app/sagas/feedback/FeedbackHistorySaga.js
@@ -7,35 +7,6 @@ import api from 'app/services/apiService';
 
 const STATUS_OK = 'ok';
 
-export function* fetchActiveJourneys() {
-  const response = yield call(api.getFeedbackPortfolio);
-  if (response.ok) {
-    if (response.data.status === STATUS_OK) {
-      const journeyList = response.data.portfolio;
-      yield put(FeedbackHistoryActions.fetchActiveJourneysSuccess(journeyList));
-    } else {
-      yield put(FeedbackHistoryActions.fetchActiveJourneysFailure(response.data));
-    }
-  } else {
-    yield put(FeedbackHistoryActions.fetchActiveJourneysFailure(response.data));
-  }
-}
-
-export function* fetchRecentJourneys() {
-  const response = yield call(api.getClosedFeedbackJourneys);
-  if (response.ok) {
-    if (response.data.status === STATUS_OK) {
-      const recentList = response.data.details;
-      yield put(FeedbackHistoryActions.fetchRecentJourneysSuccess(recentList));
-    } else {
-      yield put(
-        FeedbackHistoryActions.fetchRecentJourneysFailure(response.data),
-      );
-    }
-  } else {
-    yield put(FeedbackHistoryActions.fetchRecentJourneysFailure());
-  }
-}
 
 
 export function* fetchUserActivity() {
@@ -72,14 +43,6 @@ export function* fetchOngoingJourney() {
 }
 
 function* watchFeedbackHistorySaga() {
-  yield takeLatest(
-    FeedbackHistoryTypes.FETCH_ACTIVE_JOURNEYS,
-    fetchActiveJourneys,
-  );
-  yield takeLatest(
-    FeedbackHistoryTypes.FETCH_RECENT_JOURNEYS,
-    fetchRecentJourneys,
-  );
   yield takeLatest(
     FeedbackHistoryTypes.FETCH_USER_ACTIVITY, fetchUserActivity
   );
