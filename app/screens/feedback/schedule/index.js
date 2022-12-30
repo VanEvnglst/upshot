e24b77a28ddfdf9b9403daf25a0e314540efa093
Ivan <engr.ivan.evangelista@gmail.com>
2022-12-30 15:31:09 +0800

    code cleanup

--- app/screens/feedback/schedule/index.js
+++ app/screens/feedback/schedule/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -10,129 +10,182 @@ import {
   Dimensions,
   TouchableOpacity,
   TextInput,
-  Platform
+  Platform,
 } from 'react-native';
-import { 
-  DateTimePicker
-} from 'app/components'
+import { DateTimePicker } from 'app/components';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { useDispatch, useSelector } from "react-redux";
-import CaptureMomentActions from 'app/store/CaptureFeedbackMomentRedux';
+import { useDispatch, useSelector } from 'react-redux';
+import CaptureMomentActions from 'app/store/feedback/CaptureFeedbackMomentRedux';
 import styles from './styles';
 
-
-const ScheduleDiscussion = props => { 
+const ScheduleDiscussion = props => {
   const { navigation } = props;
   const dispatch = useDispatch();
-  const entryStep = useSelector(state => state.captureMoment.get('entryActiveStep'));
+  const entryStep = useSelector(state =>
+    state.captureMoment.get('entryActiveStep'),
+  );
   const dateNow = new Date();
-  const nextHour = dateNow.setTime(dateNow.getTime() + 1 * 60 * 60 * 1000)
+  const nextHour = dateNow.setTime(dateNow.getTime() + 1 * 60 * 60 * 1000);
 
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [selectedDate, setShowDate] = useState(moment(dateNow).format('ddd [·] MMM DD, YYYY'));
+  const [selectedDate, setShowDate] = useState(
+    moment(dateNow).format('ddd [·] MMM DD, YYYY'),
+  );
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
-  const [startTime, setStartTime] = useState(moment(nextHour).format('hh:[00] A'));
+  const [startTime, setStartTime] = useState(
+    moment(nextHour).format('hh:[00] A'),
+  );
   const [endTime, setEndTime] = useState(moment(nextHour).format('hh:[30] A'));
   const [timePickerID, setTimePickerID] = useState('');
   const [locationDetails, setLocationDetails] = useState('');
 
-  const handleScheduleDiscussion = () => { 
+  const handleScheduleDiscussion = () => {
     const scheduleDetails = {
       date: selectedDate,
       startTime: startTime,
       endTime: endTime,
-      location: locationDetails
+      location: locationDetails,
     };
-    
-    dispatch(CaptureMomentActions.setEntryActiveStep(entryStep + 1));
-    dispatch(CaptureMomentActions.setFeedbackMomentData('f2fSchedule', scheduleDetails))
-    navigation.navigate('Record Feedback Entry');
-  }
 
-  const handleDateConfirm = (date) => { 
+    dispatch(CaptureMomentActions.setEntryActiveStep(entryStep + 1));
+    dispatch(
+      CaptureMomentActions.setFeedbackMomentData(
+        'f2fSchedule',
+        scheduleDetails,
+      ),
+    );
+    navigation.navigate('Record Feedback Entry');
+  };
+
+  const handleDateConfirm = date => {
     setShowDate(moment(date).format('ddd [·] MMM DD, YYYY'));
     setDatePickerVisibility(false);
-  }
+  };
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
   };
 
-  const hideDatePicker = () => { 
+  const hideDatePicker = () => {
     setDatePickerVisibility(false);
-  }
+  };
 
-  const showTimePicker = (id) => {
+  const showTimePicker = id => {
     setTimePickerVisibility(true);
     setTimePickerID(id);
   };
 
-  const hideTimePicker = () => { 
+  const hideTimePicker = () => {
     setTimePickerVisibility(false);
     setTimePickerID('');
-  }
+  };
 
-  const handleTimeConfirm = (confirmTime ,id) => { 
+  const handleTimeConfirm = (confirmTime, id) => {
     if (id === 'startTimeID') {
       setStartTime(moment(confirmTime).format('hh:mm A'));
       setTimePickerVisibility(false);
       setTimePickerID('');
-    }
-    else if (id === 'endTimeID') { 
+    } else if (id === 'endTimeID') {
       setEndTime(moment(confirmTime).format('hh:mm A'));
       setTimePickerVisibility(false);
       setTimePickerID('');
     }
-  }
+  };
 
-  const handleTextChange = (newText) => { 
-    setLocationDetails(newText)
-  }
+  const handleTextChange = newText => {
+    setLocationDetails(newText);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-     
       <View style={styles.headerContainer}>
         <TouchableOpacity
           accessibilityRole="button"
-          onPress={() => { }}
+          onPress={() => {}}
           style={styles.icon}>
-          <Icon name="chevron-back-outline" size={24} color='#FFFFFF' />
+          <Icon name="chevron-back-outline" size={24} color="#FFFFFF" />
         </TouchableOpacity>
         <View style={styles.headerTextContainer}>
           <Text style={styles.headerText}>Schedule Discussion</Text>
         </View>
-      
+
         <View style={styles.headerSave}>
           <TouchableOpacity
-          acccessibilityRole='button'
-        onPress={() => navigation.navigate('Home')}> 
+            acccessibilityRole="button"
+            onPress={() => navigation.navigate('Home')}>
             <Text style={styles.saveText}>X</Text>
-            </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
+        </View>
       </View>
 
       <View style={styles.infoContainer}>
-        <Text style={styles.nameText} >Andre Castro</Text>
+        <Text style={styles.nameText}>Andre Castro</Text>
         <Text style={styles.infoHeaderTitle}>Set a 1-on-1</Text>
-        <Text style={styles.infoSubHeader}>Schedule a time to discuss the feedback</Text>
+        <Text style={styles.infoSubHeader}>
+          Schedule a time to discuss the feedback
+        </Text>
       </View>
 
-      <View style={{margin: 24, borderTopWidth: 1, borderTopColor: '#B1B5C3'}}>
-        <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 25}}>
-          <Icon name="calendar-outline" size={24} color='#777E90'/>
-          <Text style={{ color: '#FFFFFF', fontWeight: '700', fontSize: 16, lineHeight: 24, paddingLeft: 5}}>Schedule Details</Text>
+      <View
+        style={{ margin: 24, borderTopWidth: 1, borderTopColor: '#B1B5C3' }}>
+        <View
+          style={{ flexDirection: 'row', alignItems: 'center', marginTop: 25 }}>
+          <Icon name="calendar-outline" size={24} color="#777E90" />
+          <Text
+            style={{
+              color: '#FFFFFF',
+              fontWeight: '700',
+              fontSize: 16,
+              lineHeight: 24,
+              paddingLeft: 5,
+            }}>
+            Schedule Details
+          </Text>
         </View>
 
         <View style={{ marginTop: 24 }}>
-          <Text style={{ color: '#B1B5C3', fontWeight: '700', fontSize: 12, lineHeight: 12, textTransform: 'uppercase' }}>Date</Text>
+          <Text
+            style={{
+              color: '#B1B5C3',
+              fontWeight: '700',
+              fontSize: 12,
+              lineHeight: 12,
+              textTransform: 'uppercase',
+            }}>
+            Date
+          </Text>
           <TouchableOpacity onPress={() => showDatePicker()}>
-          <View style={{ minWidth: 327, height: 48, flexDirection: 'row', justifyContent: 'space-between', borderWidth: 2, borderColor: '#353945', borderRadius: 12, alignItems: 'center', marginTop: 12}}>
-              <Text style={{ color: '#FCFCFD', fontWeight: '500', fontSize: 14, lineHeight: 24, marginLeft: 18 }}>{selectedDate}</Text>
-            <Icon name="chevron-down-circle-outline" size={24} color='#777E90' style={{marginRight: 10}} />
+            <View
+              style={{
+                minWidth: 327,
+                height: 48,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                borderWidth: 2,
+                borderColor: '#353945',
+                borderRadius: 12,
+                alignItems: 'center',
+                marginTop: 12,
+              }}>
+              <Text
+                style={{
+                  color: '#FCFCFD',
+                  fontWeight: '500',
+                  fontSize: 14,
+                  lineHeight: 24,
+                  marginLeft: 18,
+                }}>
+                {selectedDate}
+              </Text>
+              <Icon
+                name="chevron-down-circle-outline"
+                size={24}
+                color="#777E90"
+                style={{ marginRight: 10 }}
+              />
             </View>
-            </TouchableOpacity>
+          </TouchableOpacity>
         </View>
 
         <View>
@@ -143,27 +196,105 @@ const ScheduleDiscussion = props => {
             minimumDate={new Date()}
             onCancel={() => hideDatePicker()}
             onConfirm={handleDateConfirm}
-            />
+          />
         </View>
 
-        <View style={{ marginTop: 24, minWidth: 327, flexDirection: 'row', justifyContent: 'flex-start' }}>
-          <View style={{minWidth: 157.5, justifyContent: 'flex-start' }}>
-          <Text style={{ color: '#B1B5C3', fontWeight: '700', fontSize: 12, lineHeight: 12, textTransform: 'uppercase', width: 157.5 }}>Start time</Text>
+        <View
+          style={{
+            marginTop: 24,
+            minWidth: 327,
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+          }}>
+          <View style={{ minWidth: 157.5, justifyContent: 'flex-start' }}>
+            <Text
+              style={{
+                color: '#B1B5C3',
+                fontWeight: '700',
+                fontSize: 12,
+                lineHeight: 12,
+                textTransform: 'uppercase',
+                width: 157.5,
+              }}>
+              Start time
+            </Text>
             <TouchableOpacity onPress={() => showTimePicker('startTimeID')}>
-            <View style={{ height: 48, flexDirection: 'row', justifyContent: 'space-between', borderWidth: 2, borderColor: '#353945', borderRadius: 12, alignItems: 'center', marginTop: 12 }}>
-                <Text style={{ color: '#FCFCFD', fontWeight: '500', fontSize: 14, lineHeight: 24, marginLeft: 18 }}>{startTime}</Text>
-            <Icon name="chevron-down-circle-outline" size={24} color='#777E90' style={{marginRight: 10}} />
+              <View
+                style={{
+                  height: 48,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  borderWidth: 2,
+                  borderColor: '#353945',
+                  borderRadius: 12,
+                  alignItems: 'center',
+                  marginTop: 12,
+                }}>
+                <Text
+                  style={{
+                    color: '#FCFCFD',
+                    fontWeight: '500',
+                    fontSize: 14,
+                    lineHeight: 24,
+                    marginLeft: 18,
+                  }}>
+                  {startTime}
+                </Text>
+                <Icon
+                  name="chevron-down-circle-outline"
+                  size={24}
+                  color="#777E90"
+                  style={{ marginRight: 10 }}
+                />
               </View>
-              </TouchableOpacity>
+            </TouchableOpacity>
           </View>
-          <View style={{minWidth: 157.5, justifyContent: 'flex-start', marginLeft: 12  }}>
-            <Text style={{ color: '#B1B5C3', fontWeight: '700', fontSize: 12, lineHeight: 12, textTransform: 'uppercase' }}>End time</Text>
+          <View
+            style={{
+              minWidth: 157.5,
+              justifyContent: 'flex-start',
+              marginLeft: 12,
+            }}>
+            <Text
+              style={{
+                color: '#B1B5C3',
+                fontWeight: '700',
+                fontSize: 12,
+                lineHeight: 12,
+                textTransform: 'uppercase',
+              }}>
+              End time
+            </Text>
             <TouchableOpacity onPress={() => showTimePicker('endTimeID')}>
-          <View style={{ height: 48, flexDirection: 'row', justifyContent: 'space-between', borderWidth: 2, borderColor: '#353945', borderRadius: 12, alignItems: 'center', marginTop: 12}}>
-              <Text style={{ color: '#FCFCFD', fontWeight: '500', fontSize: 14, lineHeight: 24, marginLeft: 18 }}>{ endTime }</Text>
-            <Icon name="chevron-down-circle-outline" size={24} color='#777E90' style={{marginRight: 10}} />
+              <View
+                style={{
+                  height: 48,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  borderWidth: 2,
+                  borderColor: '#353945',
+                  borderRadius: 12,
+                  alignItems: 'center',
+                  marginTop: 12,
+                }}>
+                <Text
+                  style={{
+                    color: '#FCFCFD',
+                    fontWeight: '500',
+                    fontSize: 14,
+                    lineHeight: 24,
+                    marginLeft: 18,
+                  }}>
+                  {endTime}
+                </Text>
+                <Icon
+                  name="chevron-down-circle-outline"
+                  size={24}
+                  color="#777E90"
+                  style={{ marginRight: 10 }}
+                />
               </View>
-              </TouchableOpacity>
+            </TouchableOpacity>
           </View>
 
           <DateTimePicker
@@ -171,33 +302,79 @@ const ScheduleDiscussion = props => {
             mode={'time'}
             display={'spinner'}
             onCancel={() => hideTimePicker()}
-            onConfirm={(confirmTime) => handleTimeConfirm(confirmTime, timePickerID)}
+            onConfirm={confirmTime =>
+              handleTimeConfirm(confirmTime, timePickerID)
+            }
             date={dateNow}
-            />
-
+          />
         </View>
 
-        <View style={{marginTop: 24}}>
-          <Text style={{ color: '#B1B5C3', fontWeight: '700', fontSize: 12, lineHeight: 12, textTransform: 'uppercase'}}>Location</Text>
-          <View style={{ minWidth: 327, height: 48, justifyContent: 'center', borderWidth: 2, borderColor: '#353945', borderRadius: 12, marginTop: 12, textAlignVertical: 'center'}}>
-            <TextInput style={{ color: '#FCFCFD', fontWeight: '500', fontSize: 14, lineHeight: 24, marginLeft: 18, textAlign: 'left', alignItems: 'center', height: 32}}
+        <View style={{ marginTop: 24 }}>
+          <Text
+            style={{
+              color: '#B1B5C3',
+              fontWeight: '700',
+              fontSize: 12,
+              lineHeight: 12,
+              textTransform: 'uppercase',
+            }}>
+            Location
+          </Text>
+          <View
+            style={{
+              minWidth: 327,
+              height: 48,
+              justifyContent: 'center',
+              borderWidth: 2,
+              borderColor: '#353945',
+              borderRadius: 12,
+              marginTop: 12,
+              textAlignVertical: 'center',
+            }}>
+            <TextInput
+              style={{
+                color: '#FCFCFD',
+                fontWeight: '500',
+                fontSize: 14,
+                lineHeight: 24,
+                marginLeft: 18,
+                textAlign: 'left',
+                alignItems: 'center',
+                height: 32,
+              }}
               placeholder={'Type your location...'}
-              placeholderTextColor='#777E90'
+              placeholderTextColor="#777E90"
               value={locationDetails}
-              textAlignVertical='top'
-              onChangeText={newText => handleTextChange(newText)} />
+              textAlignVertical="top"
+              onChangeText={newText => handleTextChange(newText)}
+            />
           </View>
         </View>
 
-        <TouchableOpacity style={{ backgroundColor: '#FFFFFF', minWidth: 351, height: 48, borderRadius: 12, justifyContent: 'center', alignItems: 'center', marginTop: 150 }}
-        onPress={() => handleScheduleDiscussion()}>
-          <Text style={{color: '#353945', fontWeight: '700', fontSize: 16, lineHeight: 16}}>Schedule Discussion</Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#FFFFFF',
+            minWidth: 351,
+            height: 48,
+            borderRadius: 12,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 150,
+          }}
+          onPress={() => handleScheduleDiscussion()}>
+          <Text
+            style={{
+              color: '#353945',
+              fontWeight: '700',
+              fontSize: 16,
+              lineHeight: 16,
+            }}>
+            Schedule Discussion
+          </Text>
         </TouchableOpacity>
       </View>
-      
-
-      </SafeAreaView>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
 export default ScheduleDiscussion;
